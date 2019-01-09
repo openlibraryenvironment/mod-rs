55ce479c9e193e3473f11c7a640bc946600cff8e
Ian Ibbotson <ianibbo@googlemail.com>
2019-01-09 10:12:31 +0000

    Added okapi scripts

---
+++ okapi-scripts/register_and_enable.sh
@@ -0,0 +1,28 @@
+BASEDIR=$(dirname "$0")
+# echo Please make sure you have run ./gradlew clean generateDescriptors before starting this script
+pushd "$BASEDIR/.."
+
+# Check for decriptor target directory.
+
+DESCRIPTORDIR="build/resources/main/okapi"
+
+if [ ! -d "$DESCRIPTORDIR" ]; then
+    echo "No descriptors found. Let's try building them."
+    ./gradlew generateDescriptors
+fi
+
+DEP_DESC=`cat ${DESCRIPTORDIR}/DeploymentDescriptor.json`
+SVC_ID=`echo $DEP_DESC | jq -rc '.srvcId'`
+INS_ID=`echo $DEP_DESC | jq -rc '.instId'`
+
+echo Remove any existing module ${SVC_ID}/${INS_ID}
+curl -XDELETE "http://localhost:9130/_/proxy/tenants/diku/modules/${SVC_ID}"
+curl -XDELETE "http://localhost:9130/_/discovery/modules/${SVC_ID}/${INS_ID}"
+curl -XDELETE "http://localhost:9130/_/proxy/modules/${SVC_ID}"
+
+# ./gradlew clean generateDescriptors
+echo Install latest module ${SVC_ID}/${INS_ID}
+curl -XPOST http://localhost:9130/_/proxy/modules -d @"${DESCRIPTORDIR}/ModuleDescriptor.json"
+curl -XPOST http://localhost:9130/_/discovery/modules -d "$DEP_DESC"
+curl -XPOST http://localhost:9130/_/proxy/tenants/diku/modules -d `echo $DEP_DESC | jq -rc '{id: .srvcId}'`
+popd
