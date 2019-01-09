efc237e12cf8b7b7ef88d6291b3dc1e1dd87082f
Ian Ibbotson <ianibbo@googlemail.com>
2019-01-09 10:02:52 +0000

    Initial run of schema generation

--- grails-app/migrations/initial-model.groovy
+++ grails-app/migrations/initial-model.groovy
@@ -1,2 +1,286 @@
 databaseChangeLog = {
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-1") {
+        createSequence(sequenceName: "hibernate_sequence")
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-2") {
+        createTable(tableName: "custom_property") {
+            column(autoIncrement: "true", name: "id", type: "BIGINT") {
+                constraints(primaryKey: "true", primaryKeyName: "custom_propertyPK")
+            }
+
+            column(name: "version", type: "BIGINT") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "definition_id", type: "VARCHAR(36)")
+
+            column(name: "parent_id", type: "BIGINT")
+        }
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-3") {
+        createTable(tableName: "custom_property_blob") {
+            column(name: "id", type: "BIGINT") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "value", type: "OID") {
+                constraints(nullable: "false")
+            }
+        }
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-4") {
+        createTable(tableName: "custom_property_boolean") {
+            column(name: "id", type: "BIGINT") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "value", type: "BOOLEAN") {
+                constraints(nullable: "false")
+            }
+        }
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-5") {
+        createTable(tableName: "custom_property_container") {
+            column(name: "id", type: "BIGINT") {
+                constraints(nullable: "false")
+            }
+        }
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-6") {
+        createTable(tableName: "custom_property_decimal") {
+            column(name: "id", type: "BIGINT") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "value", type: "NUMBER(19, 2)") {
+                constraints(nullable: "false")
+            }
+        }
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-7") {
+        createTable(tableName: "custom_property_definition") {
+            column(name: "pd_id", type: "VARCHAR(36)") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "version", type: "BIGINT") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "pd_name", type: "VARCHAR(255)") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "pd_type", type: "VARCHAR(255)") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "pd_description", type: "VARCHAR(255)")
+        }
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-8") {
+        createTable(tableName: "custom_property_integer") {
+            column(name: "id", type: "BIGINT") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "value", type: "INT") {
+                constraints(nullable: "false")
+            }
+        }
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-9") {
+        createTable(tableName: "custom_property_refdata") {
+            column(name: "id", type: "BIGINT") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "value_id", type: "VARCHAR(36)") {
+                constraints(nullable: "false")
+            }
+        }
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-10") {
+        createTable(tableName: "custom_property_refdata_definition") {
+            column(name: "pd_id", type: "VARCHAR(255)") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "category_id", type: "VARCHAR(36)") {
+                constraints(nullable: "false")
+            }
+        }
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-11") {
+        createTable(tableName: "custom_property_text") {
+            column(name: "id", type: "BIGINT") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "value", type: "CLOB") {
+                constraints(nullable: "false")
+            }
+        }
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-12") {
+        createTable(tableName: "patron_request") {
+            column(name: "pr_id", type: "VARCHAR(36)") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "version", type: "BIGINT") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "title", type: "VARCHAR(255)") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "pr_service_type_fk", type: "VARCHAR(36)") {
+                constraints(nullable: "false")
+            }
+        }
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-13") {
+        createTable(tableName: "refdata_category") {
+            column(name: "rdc_id", type: "VARCHAR(36)") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "rdc_version", type: "BIGINT") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "rdc_description", type: "VARCHAR(255)") {
+                constraints(nullable: "false")
+            }
+        }
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-14") {
+        createTable(tableName: "refdata_value") {
+            column(name: "rdv_id", type: "VARCHAR(36)") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "rdv_version", type: "BIGINT") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "rdv_value", type: "VARCHAR(255)") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "rdv_owner", type: "VARCHAR(36)") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "rdv_label", type: "VARCHAR(255)") {
+                constraints(nullable: "false")
+            }
+        }
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-15") {
+        addPrimaryKey(columnNames: "id", constraintName: "custom_property_blobPK", tableName: "custom_property_blob")
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-16") {
+        addPrimaryKey(columnNames: "id", constraintName: "custom_property_booleanPK", tableName: "custom_property_boolean")
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-17") {
+        addPrimaryKey(columnNames: "id", constraintName: "custom_property_containerPK", tableName: "custom_property_container")
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-18") {
+        addPrimaryKey(columnNames: "id", constraintName: "custom_property_decimalPK", tableName: "custom_property_decimal")
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-19") {
+        addPrimaryKey(columnNames: "pd_id", constraintName: "custom_property_definitionPK", tableName: "custom_property_definition")
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-20") {
+        addPrimaryKey(columnNames: "id", constraintName: "custom_property_integerPK", tableName: "custom_property_integer")
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-21") {
+        addPrimaryKey(columnNames: "id", constraintName: "custom_property_refdataPK", tableName: "custom_property_refdata")
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-22") {
+        addPrimaryKey(columnNames: "pd_id", constraintName: "custom_property_refdata_definitionPK", tableName: "custom_property_refdata_definition")
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-23") {
+        addPrimaryKey(columnNames: "id", constraintName: "custom_property_textPK", tableName: "custom_property_text")
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-24") {
+        addPrimaryKey(columnNames: "pr_id", constraintName: "patron_requestPK", tableName: "patron_request")
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-25") {
+        addPrimaryKey(columnNames: "rdc_id", constraintName: "refdata_categoryPK", tableName: "refdata_category")
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-26") {
+        addPrimaryKey(columnNames: "rdv_id", constraintName: "refdata_valuePK", tableName: "refdata_value")
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-27") {
+        addUniqueConstraint(columnNames: "pd_name", constraintName: "UC_CUSTOM_PROPERTY_DEFINITIONPD_NAME_COL", tableName: "custom_property_definition")
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-28") {
+        createIndex(indexName: "rdv_entry_idx", tableName: "refdata_value") {
+            column(name: "rdv_value")
+
+            column(name: "rdv_owner")
+        }
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-29") {
+        createIndex(indexName: "td_type_idx", tableName: "custom_property_definition") {
+            column(name: "pd_type")
+        }
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-30") {
+        addForeignKeyConstraint(baseColumnNames: "definition_id", baseTableName: "custom_property", constraintName: "FK36grvth72fb7wu5i5xaeqjitw", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "pd_id", referencedTableName: "custom_property_definition")
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-31") {
+        addForeignKeyConstraint(baseColumnNames: "pr_service_type_fk", baseTableName: "patron_request", constraintName: "FK5fk9w5hr3e7s3f2kg9t4acnmm", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "rdv_id", referencedTableName: "refdata_value")
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-32") {
+        addForeignKeyConstraint(baseColumnNames: "value_id", baseTableName: "custom_property_refdata", constraintName: "FK5ogn0fedwxxy4fhmq9du4qej2", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "rdv_id", referencedTableName: "refdata_value")
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-33") {
+        addForeignKeyConstraint(baseColumnNames: "category_id", baseTableName: "custom_property_refdata_definition", constraintName: "FKbrh88caagajlvrpaydg4tr3qx", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "rdc_id", referencedTableName: "refdata_category")
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-34") {
+        addForeignKeyConstraint(baseColumnNames: "parent_id", baseTableName: "custom_property", constraintName: "FKd5u2tgpracxvk1xw8pdreuj5h", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "custom_property_container")
+    }
+
+    changeSet(author: "ibbo (generated)", id: "1547027929284-35") {
+        addForeignKeyConstraint(baseColumnNames: "rdv_owner", baseTableName: "refdata_value", constraintName: "FKh4fon2a7k4y8b2sicjm0i6oy8", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "rdc_id", referencedTableName: "refdata_category")
+    }
 }
