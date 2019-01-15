e3ca3e1b2d957190b63fc20eda7691bd647cb348
Ian Ibbotson <ianibbo@googlemail.com>
2019-01-15 12:54:43 +0000

    updated module descriptor, added test cases for refdata, added config for refdata and custprops mappings

--- grails-app/controllers/mod/rs/UrlMappings.groovy
+++ grails-app/controllers/mod/rs/UrlMappings.groovy
@@ -10,6 +10,17 @@ class UrlMappings {
 
     "/chas/$action" ( controller: "Chas")
 
+    // Call /rs/refdata to list all refdata categories
+    '/rs/refdata/'(resources: 'refdata') {
+      collection {
+        "/$domain/$property" (controller: 'refdata', action: 'lookup')
+      }
+    }
+
+    // Call /rs/custprop  to list all custom properties
+    '/rs/custprops'(resources: 'customPropertyDefinition')
+
+
     "500"(view: '/error')
     "404"(view: '/notFound')
   }
