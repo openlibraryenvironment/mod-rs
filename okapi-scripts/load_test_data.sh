05a730146e3f6b7f838dea69b1ad7673cc3cce75
Ian Ibbotson <ianibbo@googlemail.com>
2019-01-15 11:35:25 +0000

    Working addition of patronrequest in load_test_data.sh script

--- okapi-scripts/load_test_data.sh
+++ okapi-scripts/load_test_data.sh
@@ -2,6 +2,14 @@
 
 AUTH_TOKEN=`./okapi-login`
 
+echo Listing current requests
+curl --header "X-Okapi-Tenant: diku" -H "X-Okapi-Token: ${AUTH_TOKEN}" -H "Content-Type: application/json" -X GET http://localhost:9130/rs/patronrequests 
+
+
 PATRON_REQ_1=`curl --header "X-Okapi-Tenant: diku" -H "X-Okapi-Token: ${AUTH_TOKEN}" -H "Content-Type: application/json" -X POST http://localhost:9130/rs/patronrequests -d ' {
+  title:"Brain of the firm",
+  patronReference:"Iantest112",
+  serviceType:"Loan",
+  state:"Idle"
 }
-'
+'`
--- okapi-scripts/non_okapi_test.sh
+++ okapi-scripts/non_okapi_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl --header "X-Okapi-Tenant: diku" -H "Content-Type: application/json" http://localhost:8080/rs/patronRequests -X GET
+curl --header "X-Okapi-Tenant: diku" -H "Content-Type: application/json" http://localhost:8080/rs/patronrequests -X GET
