d936faa003cf7de17668b15c0f83a63f619b1f56
Chas <chas.woodfield@k-int.com>
2019-01-16 13:34:24 +0000

    Commented out the code in application.groovy

--- grails-app/init/mod/rs/Application.groovy
+++ grails-app/init/mod/rs/Application.groovy
@@ -10,10 +10,10 @@ import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import springfox.documentation.RequestHandler
-import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.RequestHandler
+//import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.grails.SpringfoxGrailsIntegrationConfiguration;
-import springfox.documentation.service.ApiInfo
+//import springfox.documentation.service.ApiInfo
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -21,10 +21,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import static springfox.documentation.builders.PathSelectors.ant;
 
 // Enable SpringFox on your project
-@EnableSwagger2
+//@EnableSwagger2
 
 // Import the springfox grails integration configuration
-@Import([springfox.documentation.grails.SpringfoxGrailsIntegrationConfiguration])
+//@Import([SpringfoxGrailsIntegrationConfiguration])
 
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
@@ -33,46 +33,49 @@ class Application extends GrailsAutoConfiguration {
 
 	// 3. **Optionally** define a custom docket or omit this step to use the default
 	// For grails it is preferrable to use use the following settings.
-	@Bean
-	Docket api() {
+//	@Bean
+//	Docket api() {
 //	  new Docket(DocumentationType.SWAGGER_2)
 //		  .ignoredParameterTypes(MetaClass)
 //		  .select()
 //		  .paths(Predicates.not(ant("/error")))
-//		  .build()
-		return new Docket(DocumentationType.SWAGGER_2).apiInfo(ApiInfo.DEFAULT).select() 
-//          .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
-          .apis(IgnoreValidateController())
-//		  .paths(Predicates.not(ant("/error")))
-             .build();
+//		  .build();
 
-	}
+//		return new Docket(DocumentationType.SWAGGER_2).apiInfo(ApiInfo.DEFAULT).select() 
+//          .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
+//		  .paths(Predicates.not(ant("/error")))
+//             .build();
+//
+//		return new Docket(DocumentationType.SWAGGER_2).apiInfo(ApiInfo.DEFAULT).select() 
+//          .apis(IgnoreValidateController())
+//             .build();
+//	}
   
-   //  **Optionally** in the absense of asset pipeline configure the swagger-ui webjar to serve the scaffolded swagger UI
-    @Bean 
-    static WebMvcConfigurerAdapter webConfigurer() { 
-        new WebMvcConfigurerAdapter() { 
-            @Override 
-            void addResourceHandlers(ResourceHandlerRegistry registry) { 
-                if (!registry.hasMappingForPattern("/webjars/**")) { 
-                    registry.addResourceHandler("/webjars/**") 
-                            .addResourceLocations("classpath:/META-INF/resources/webjars/") 
-                } 
-                if (!registry.hasMappingForPattern("/swagger-ui.html")) { 
-                    registry.addResourceHandler("/swagger-ui.html") 
-                            .addResourceLocations("classpath:/META-INF/resources/swagger-ui.html") 
-                } 
-            } 
-        } 
-    }
+    //  **Optionally** in the absense of asset pipeline configure the swagger-ui webjar to serve the scaffolded swagger UI
+//    @Bean 
+//    static WebMvcConfigurerAdapter webConfigurer() { 
+//        new WebMvcConfigurerAdapter() { 
+//            @Override 
+//            void addResourceHandlers(ResourceHandlerRegistry registry) { 
+//                if (!registry.hasMappingForPattern("/webjars/**")) { 
+//                    registry.addResourceHandler("/webjars/**") 
+//                            .addResourceLocations("classpath:/META-INF/resources/webjars/") 
+//                } 
+//                if (!registry.hasMappingForPattern("/swagger-ui.html")) { 
+//                    registry.addResourceHandler("/swagger-ui.html") 
+//                            .addResourceLocations("classpath:/META-INF/resources/swagger-ui.html") 
+//                } 
+//            } 
+//        } 
+//    }
 	 
-	public static Predicate<RequestHandler> IgnoreValidateController() {
-		return new Predicate<RequestHandler>() {
-			@Override
-			public boolean apply(RequestHandler input) {
-				boolean result = input.getName() != "com.k_int.web.toolkit.ValidateController";
-				return result;
-			}
-		};
-	}
+//	public static Predicate<RequestHandler> IgnoreValidateController() {
+//		return new Predicate<RequestHandler>() {
+//			@Override
+//			public boolean apply(RequestHandler input) {
+//				boolean result = input.getName() != "com.k_int.web.toolkit.ValidateController";
+//				return result;
+//			}
+//		};
+//	}
 }
