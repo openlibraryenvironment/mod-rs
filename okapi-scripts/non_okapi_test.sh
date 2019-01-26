716fb80dc9b2fd7d4ff8a2421dd14442710ee313
Ian Ibbotson <ianibbo@googlemail.com>
2019-01-26 14:16:48 +0000

    Added extra logging config

--- okapi-scripts/non_okapi_test.sh
+++ okapi-scripts/non_okapi_test.sh
@@ -3,3 +3,4 @@
 curl --header "X-Okapi-Tenant: diku" -H "Content-Type: application/json" http://localhost:8080/rs/patronrequests -X GET
 curl --header "X-Okapi-Tenant: diku" -H "Content-Type: application/json" http://localhost:8080/rs/custprops -X GET
 curl --header "X-Okapi-Tenant: diku" -H "Content-Type: application/json" http://localhost:8080/rs/refdata -X GET
+curl --header "X-Okapi-Tenant: diku" -H "Content-Type: application/json" http://localhost:8080/chas/TestXML -X GET
