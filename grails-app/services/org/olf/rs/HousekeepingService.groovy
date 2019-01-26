24dc9f7a27105d65b08f9285c110044cae1fb965
Ian Ibbotson <ianibbo@googlemail.com>
2019-01-26 14:22:57 +0000

    Added org/olf/rs/HousekeepingService as a place to put tenant initialisation commands. Wired in to events

---
+++ grails-app/services/org/olf/rs/HousekeepingService.groovy
@@ -0,0 +1,38 @@
+package org.olf.rs
+
+import grails.gorm.multitenancy.Tenants
+import grails.events.annotation.Subscriber
+import grails.gorm.multitenancy.WithoutTenant
+import grails.gorm.transactions.Transactional
+
+/**
+ * This service works at the module level, it's often called without a tenant context.
+ */
+@Transactional
+public class HousekeepingService {
+
+  /**
+   * This is called by the eventing mechanism - There is no web request context
+   * this method is called after the schema for a tenant is updated.
+   */
+  @Subscriber('okapi:schema_update')
+  public void onSchemaUpdate(tn, tid) {
+    log.debug("HousekeepingService::onSchemaUpdate(${tn},${tid})")
+    setupData(tn, tid);
+  }
+
+  /**
+   * Put calls to estabish any required reference data in here. This method MUST be communtative - IE repeated calls must leave the 
+   * system in the same state. It will be called regularly throughout the lifecycle of a project. It is common to see calls to
+   * lookupOrCreate, or "upsert" type functions in here."
+   */
+  private void setupData(tenantName, tenantId) {
+
+    log.info("HousekeepingService::setupData(${tenantName},${tenantId})");
+
+    // Establish a database session in the context of the activated tenant. You can use GORM domain classes inside the closure
+    Tenants.withId(tenantId) {
+    }
+  }
+
+}
