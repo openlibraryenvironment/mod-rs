4c217cefd4c093a4b01885116bd67fbdcbfa4ae1
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2019-02-22 13:52:00 +0000

    Mapping context per domain object. Enhance them all.

--- grails-app/init/mod/rs/Application.groovy
+++ grails-app/init/mod/rs/Application.groovy
@@ -5,9 +5,10 @@ import grails.boot.config.GrailsAutoConfiguration;
 import org.grails.datastore.gorm.validation.constraints.registry.DefaultConstraintRegistry;
 import org.grails.datastore.gorm.validation.constraints.registry.ConstraintRegistry;
 import org.grails.datastore.mapping.model.MappingContext;
+import org.grails.datastore.mapping.model.PersistentEntity
 import org.grails.datastore.gorm.validation.constraints.registry.DefaultValidatorRegistry;
 import grails.util.Holders;
-import org.olf.rs.PendingActionValidator;
+import org.olf.rs.PendingActionConstraint;
 
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
@@ -15,12 +16,17 @@ class Application extends GrailsAutoConfiguration {
     }
 
     void doWithApplicationContext() {
-		try {
-			MappingContext mc = grailsApplication.mainContext.getBean('grailsDomainClassMappingContext')
-			DefaultValidatorRegistry reg = mc.validatorRegistry
-			reg.constraintRegistry.addConstraint(PendingActionValidator.class);
-		} catch (Exception e) {
-			def chas = 1;
-		}
+  		try {
+  			MappingContext mc = grailsApplication.mainContext.getBean('grailsDomainClassMappingContext')
+        DefaultValidatorRegistry reg = mc.validatorRegistry
+        reg.constraintRegistry.addConstraint(PendingActionConstraint.class)
+                
+        mc.getPersistentEntities().each { PersistentEntity pe ->
+          reg = pe.mappingContext.validatorRegistry
+          reg.constraintRegistry.addConstraint(PendingActionConstraint.class)
+        }
+  		} catch (Exception e) {
+  			def chas = 1;
+  		}
     }
 }
