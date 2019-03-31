4803e787bbaf5559944560e290c922a842898601
Ian Ibbotson <ianibbo@gmail.com>
2019-03-31 11:31:55 +0100

    Slightly more explicit log message eg Perform action ILL for symbol RESHARE:DIKUA who resides in tenant diku

--- grails-app/rabbit-consumers/org/olf/rs/RSMSConsumer.groovy
+++ grails-app/rabbit-consumers/org/olf/rs/RSMSConsumer.groovy
@@ -38,7 +38,8 @@ class RSMSConsumer {
       log.debug("Message is bound for ${context.envelope.routingKey}");
 
       // Strip off "RSInboundMessage."
-      String symbol = context.envelope.routingKey.substring(17);
+      // String symbol = context.envelope.routingKey.substring(17);
+      String symbol = body.participantInfo.recipient.institution_symbol;
 
       // See if we have a tenant registered for that symbol
       String tenant = housekeepingService.findTenantForSymbol(symbol);
@@ -46,6 +47,13 @@ class RSMSConsumer {
       log.debug("Result of resolve tenant for ${symbol} : ${tenant}");
       // We've got a tenant, so now we need to issue an event to that tennant - this might involve
       // creating a request, or updating the state of an existing request.
+
+      // Resource sharing message services arranges for us to have some extra properties in the body:
+      // participantInfo:[sender:[institution_symbol:RESHARE:ACMAIN], 
+      //   sender_event:ILLreq, 
+      //   recipient:[institution_symbol:RESHSARE:DIKUA], 
+      //   recipient_event:ILL]
+      log.debug("Perform action ${body.participantInfo.recipient_event} for symbol ${symbol} who resides in tenant ${tenant}");
     }
     else {
       log.debug("Body is null");
