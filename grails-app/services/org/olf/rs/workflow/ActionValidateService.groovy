16a3902f415c5cea9ef6f88a2e04dabd6cb38168
Ian Ibbotson <ianibbo@googlemail.com>
2019-05-02 09:47:42 +0100

    Get exception via getAttribute

--- grails-app/services/org/olf/rs/workflow/ActionValidateService.groovy
+++ grails-app/services/org/olf/rs/workflow/ActionValidateService.groovy
@@ -7,16 +7,16 @@ import org.olf.rs.workflow.AbstractAction.ActionResponse;
 @Transactional
 class ActionValidateService extends AbstractAction {
 
-	/** Returns the action that this class represents */
-	@Override
-	String getActionCode() {
-		return(Action.VALIDATE);
-	}
+  /** Returns the action that this class represents */
+  @Override
+  String getActionCode() {
+    return(Action.VALIDATE);
+  }
 
-	/** Performs the action */
-	@Override
-	ActionResponse perform(PatronRequest requestToBeProcessed) {
-		// For the time being we just return OK as we do not do anything
-		return(ActionResponse.SUCCESS);
-	}
+  /** Performs the action */
+  @Override
+  ActionResponse perform(PatronRequest requestToBeProcessed) {
+    // For the time being we just return OK as we do not do anything
+    return(ActionResponse.SUCCESS);
+  }
 }
