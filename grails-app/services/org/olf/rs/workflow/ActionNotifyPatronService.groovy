514ff932fc9e8480cd193d5601494822fc5ed249
Chas <chas.woodfield@k-int.com>
2019-05-15 10:29:32 +0100

    Added the action / status / transitions for the happy path

---
+++ grails-app/services/org/olf/rs/workflow/ActionNotifyPatronService.groovy
@@ -0,0 +1,25 @@
+package org.olf.rs.workflow
+
+import grails.gorm.transactions.Transactional;
+import org.olf.rs.PatronRequest;
+import org.olf.rs.workflow.AbstractAction.ActionResponse;
+import groovy.util.logging.Slf4j
+
+@Slf4j
+@Transactional
+class ActionNotifyPatronService extends AbstractAction {
+
+	/** Returns the action that this class represents */
+	@Override
+	String getActionCode() {
+		return(Action.NOTIFY_PATRON);
+	}
+
+	/** Performs the action */
+	@Override
+	ActionResponse perform(PatronRequest requestToBeProcessed) {
+		// For the time being we just return OK as we do not do anything
+		log.debug("ActionNotifyPatronService::perform(${requestToBeProcessed})");
+		return(ActionResponse.SUCCESS);
+	}
+}
