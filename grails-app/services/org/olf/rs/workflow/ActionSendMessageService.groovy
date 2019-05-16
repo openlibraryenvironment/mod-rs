6479bac371164967751f05a2873c6a813379652f
Ian Ibbotson <ianibbo@gmail.com>
2019-05-16 18:06:58 +0100

    Random stab at how we might send an 18626 message - no idea if this is whats envisaged, but a start

--- grails-app/services/org/olf/rs/workflow/ActionSendMessageService.groovy
+++ grails-app/services/org/olf/rs/workflow/ActionSendMessageService.groovy
@@ -4,10 +4,15 @@ import grails.gorm.transactions.Transactional;
 import org.olf.rs.PatronRequest;
 import org.olf.rs.workflow.AbstractAction.ActionResponse;
 import groovy.util.logging.Slf4j
+import org.olf.rs.RabbitService
+import org.olf.rs.rabbit.Queue;
 
 @Slf4j
 @Transactional
 class ActionSendMessageService extends AbstractAction {
+
+  /** The service that we use to put the message on the queue - Injected by framework */
+  RabbitService rabbitService;
 
   /** Returns the action that this class represents */
   @Override
@@ -20,6 +25,15 @@ class ActionSendMessageService extends AbstractAction {
   ActionResponse perform(PatronRequest requestToBeProcessed) {
     // For the time being we just return OK as we do not do anything
     log.debug("ActionSendMessageService::perform(${requestToBeProcessed})");
+
+    // Not sure this is what's intended, but want to start wiring together the edge module
+    // so sending a message this way for now, expect this to change when @chas gets to it.
+    rabbitService.Send(Queue.ISO18626, 
+                       java.util.UUID.randomUUID().toString(),
+                       [
+                         "stub":"stub"
+                       ]);
+
     return(ActionResponse.SUCCESS);
   }
 }
