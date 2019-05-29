f271dc627f0c85f6d9f48596a97f52ec5f8c05c2
Ian Ibbotson <ianibbo@gmail.com>
2019-05-29 11:15:12 +0100

    Working on module-to-module calls between mod-rs and mod-directory

--- grails-app/services/org/olf/rs/DirectoryService.groovy
+++ grails-app/services/org/olf/rs/DirectoryService.groovy
@@ -13,11 +13,28 @@ import org.grails.datastore.mapping.core.exceptions.ConfigurationException
 import org.grails.plugins.databasemigration.liquibase.GrailsLiquibase
 import org.olf.rs.workflow.ReShareMessageService
 
+import com.k_int.okapi.OkapiClient;
+import groovyx.net.http.FromServer
+
 /**
  * This service provides java/groovy interface to mod-directory affordances.
  */
 @Transactional
 public class DirectoryService {
+
+  OkapiClient okapiClient;
+
+  // Held and managed at the level of the service
+  String system_agent_jwt = null;
+  
+  // grab and store a long lived JWT for doing system-to-system calls without a logged in user -
+  // in this case doing a directory lookup
+  private synchronized String getSystemJwt() {
+    if ( system_agent_jwt == null ) {
+      system_agent_jwt = getToken('SystemAgent','SystemAgent');
+    }
+    return system_agent_jwt;
+  }
 
   String getIDForSymbol(String authority, String symbol) {
     // This helper needs to talk to mod-directory, and in particular, needs to return the results of
@@ -40,10 +57,48 @@ public class DirectoryService {
     //
     // Asking steve for advice as there is no incoming OKAPI request object here so authentication and authorization are problems
     //
+   
+    // Need help from @Sosguthorpe here - can't get this to work and not sure why
+    // String jwt = getSystemJwt();
+    String jwt = null;
+
+    if ( jwt ) {
+      log.error("Get directory entry");
+    }
+    else {
+      log.error("Unable to call directory service -- no token");
+    }
 
     return [
       id:'1234'
     ]
+  }
+
+  private String getToken(String user, String pass) {
+
+    String result = null;
+    Map loginDetails = [ 'username':user, 'password':pass ]
+
+    okapiClient.post('/authn/login', loginDetails, [:]) {
+      // request.setHeaders(headers)
+      response.success { FromServer fs ->
+        okapiPresent = true
+        
+        fs.getHeaders().each { FromServer.Header h ->
+          if (h.key.toLowerCase() == OkapiHeaders.TOKEN.toLowerCase()) {
+            // headers.put(h.key, h.value)
+            result = h.value;
+          }
+        }
+      }
+
+      response.failure { resp ->
+        log.warn("Error response attempting to get login for ${user} : ${resp}");
+      }
+    }
+    log.debug("Got token: ${result}");
+
+    return result
   }
 
 }
