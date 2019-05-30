d9230ba7040210f34b2bac8e7bb5d241cc2487c8
Ian Ibbotson <ianibbo@gmail.com>
2019-05-30 13:17:55 +0100

    Working ProcessorResponse invocation

--- grails-app/rabbit-consumers/org/olf/rs/ProcessorResponseConsumer.groovy
+++ grails-app/rabbit-consumers/org/olf/rs/ProcessorResponseConsumer.groovy
@@ -17,8 +17,23 @@ class ProcessorResponseConsumer {
 
   /**
    * Consumer configuration.
+   *
+   * Defining the consumer in application.yml as below did not seem to work. definint it in the static rabbitConfig did, so thats what we do
+   * Here is the config that did not work, as a signpost for others
+   *
+   * # ProcessorResponseConsumer configuration moved to the consumer itself. Defining the exchange/topic/queue binding
+   * # in this way did not work as expected. Does work as expected when specified in grails-app/rabbit-consumers/ProcessorResponseConsumer
+   * # Leaving this here as a signpost for why we did it this way.
+   * # ProcessorResponseConsumer:
+   * #   exchange: RSExchange
+   * #   binding: ProcessorResponse.#
+   * #   queue: ProcessorResponse
+   * #   match: any
+   *
    */
   static rabbitConfig = [
+    "exchange": "RSExchange",
+    "binding": "ProcessorResponse.#"
   ]
 
   /**
@@ -31,6 +46,9 @@ class ProcessorResponseConsumer {
    */
   def handleMessage(Map body, MessageContext context) {
     log.debug("**ProcessorResponse** ${body} Context:${context}");
+
+    // This is an indication that a protocol message was either sent or not, we need to move the patron request status
+    // along accordingly.
   }
 
 }
