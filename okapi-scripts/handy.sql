6983938e2373df59a1686979a9640ccaba9cbae3
Ian Ibbotson <ianibbo@gmail.com>
2019-05-30 15:36:20 +0100

    Additional handy sql

--- okapi-scripts/handy.sql
+++ okapi-scripts/handy.sql
@@ -3,3 +3,5 @@ psql -U folio_admin -h localhost okapi_modules
 set search_path to diku_mod_rs, public;
 
 select * from patron_request ;
+select pr_id, pr_date_created, pr_stitle, pr_is_requester, pr_pending_action_fk, pr_awaiting_protocol_response from patron_request;
+
