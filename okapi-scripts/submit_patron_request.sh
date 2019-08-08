24d0b4bebbedeff45106b453f985334c3d66725f
Ian Ibbotson <ianibbo@gmail.com>
2019-08-08 12:05:36 +0100

    Added requesting symbol to patron request

--- okapi-scripts/submit_patron_request.sh
+++ okapi-scripts/submit_patron_request.sh
@@ -4,6 +4,7 @@ AUTH_TOKEN=`./okapi-login`
 
 #  serviceType:"Loan",
 PATRON_REQ_1=`curl --header "X-Okapi-Tenant: diku" -H "X-Okapi-Token: ${AUTH_TOKEN}" -H "Content-Type: application/json" -X POST http://localhost:9130/rs/patronrequests -d ' {
+  requestingInstitutionSymbol:"wibble:wobble",
   title:"Platform for Change",
   author:"Beer, Stafford A",
   subtitle:"A message from Stafford Beer",
