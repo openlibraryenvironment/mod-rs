e046410e9ef0bf7156c1a2adefc28357cd18bf07
Ian Ibbotson <ianibbo@googlemail.com>
2019-08-27 12:42:02 +0100

    Working eventing

--- service/grails-app/services/org/olf/rs/ReshareApplicationEventHandlerService.groovy
+++ service/grails-app/services/org/olf/rs/ReshareApplicationEventHandlerService.groovy
@@ -1,11 +1,27 @@
 package org.olf.rs;
 
 import grails.events.annotation.Subscriber
+import groovy.lang.Closure
 
 public class ReshareApplicationEventHandlerService {
+
+  private static Map<String,Closure> handlers = [
+    'NewPatronRequest_ind':{ service, eventData ->
+      service.handleNewPatronRequestIndication(eventData);
+    }
+  ]
 
   @Subscriber('PREventIndication')
   public handleApplicationEvent(Map eventData) {
     log.debug("ReshareApplicationEventHandlerService::handleApplicationEvent(${eventData})");
+    Closure c = handlers[eventData.event]
+    if ( c != null ) {
+      // System has a closure registered for event, call it
+      c.call(this, eventData);
+    }
+  }
+
+  public void handleNewPatronRequestIndication(eventData) {
+    log.debug("ReshareApplicationEventHandlerService::handleNewPatronRequestIndication(${eventData})");
   }
 }
