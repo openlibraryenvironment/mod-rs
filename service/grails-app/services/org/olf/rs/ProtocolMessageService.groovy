e9be32f5249aa7e470bab08256d9690d1563f916
Ethan Freestone <ethan.freestone@k-int.co.uk>
2019-08-29 09:35:04 +0100

    CHanged a typo

--- service/grails-app/services/org/olf/rs/ProtocolMessageService.groovy
+++ service/grails-app/services/org/olf/rs/ProtocolMessageService.groovy
@@ -10,7 +10,7 @@ class ProtocolMessageService {
     //Make this create a new request in the responder's system
   }
   public messageConfirmation(eventData, messageType) {
-    //TODO make this able to return a confirmation message if request/supplying acency message/requesting agency message are successful,
+    //TODO make this able to return a confirmation message if request/supplying agency message/requesting agency message are successful,
     //and returning error messages if not
     return true
   }
