97792307e31b3616896cb1a91c8d2b3e9195ee1b
Ian Ibbotson <ianibbo@googlemail.com>
2019-08-30 08:43:30 +0100

    Added some hints to the code

--- service/grails-app/services/org/olf/rs/ProtocolMessageService.groovy
+++ service/grails-app/services/org/olf/rs/ProtocolMessageService.groovy
@@ -10,7 +10,7 @@ class ProtocolMessageService {
 
 
   /**
-   * @Param eventData : A map structured as followed 
+   * @param eventData : A map structured as followed 
    *   event: {
    *     envelope:{
    *       sender:{
@@ -23,27 +23,41 @@ class ProtocolMessageService {
    *       messageBody:{
    *       }
    *   }
+   *
+   * @return a map containing properties including any confirmationId the underlying protocol implementation provides us
+   *
    */
-  public void sendProtocolMessage(Map eventData) {
+  public Map sendProtocolMessage(Map eventData) {
     def responseConfirmed = messageConfirmation(eventData, "request")
     log.debug("sendRequest called for ${eventData.payload.id}")
     //Make this create a new request in the responder's system
+    String confirmation = null;
 
     // The first thing to do is to look in the internal SharedConfig to see if the recipient is a
-    // tenant in this system. If so, we can simply call the loopback interface.
+    // tenant in this system. If so, we can simply call handleIncomingMessage
+
+    return [
+      confirmationId:confirmation;
+    ]
   }
 
   /**
-   * @Param eventData Symmetrical with the section above.
+   * @param eventData Symmetrical with the section above. See para on sendProtocolMessage - Map should have exactly the same shape
+   * @return a confirmationId
    */
-  public void handleIncomingMessage(Map eventData) {
+  public Map handleIncomingMessage(Map eventData) {
     // Recipient must be a tenant in the SharedConfig
-  }
 
+    // Now we issue a protcolMessageIndication event so that any handlers written for the protocol message can be 
+    // called - this method should not do any work beyond understanding what event needs to be dispatched for the 
+    // particular message coming in.
+    return [
+      confirmationId:new java.util.UUID.randomUUID().toString();
+    ]
+  }
 
   public messageConfirmation(eventData, messageType) {
     //TODO make this able to return a confirmation message if request/supplying agency message/requesting agency message are successful,
     //and returning error messages if not
-    return true
   }
 }
