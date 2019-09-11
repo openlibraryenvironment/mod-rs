47c0b75a9a896339929efd7288afcb0f054a7313
Ian Ibbotson <ianibbo@googlemail.com>
2019-09-11 12:33:01 +0100

    Added notes

--- service/grails-app/services/org/olf/rs/HostLMSService.groovy
+++ service/grails-app/services/org/olf/rs/HostLMSService.groovy
@@ -9,7 +9,15 @@ public class HostLMSService {
   void validatePatron(String patronIdentifier) {
   }
 
+
+  /**
+   *
+   *
+   */
   void placeHold(String itemIdentifier) {
+    // For NCIP2:: issue RequestItem()
+    // RequestItem takes BibliographicId(A string, or name:value pair identifying an instance) or 
+    // ItemId(Item)(A String, or name:value pair identifying an item)
   }
 
   
