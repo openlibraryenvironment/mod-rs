cc7559e4d22b9392fee0d40fc53a257cf3b30e42
Ian Ibbotson <ianibbo@googlemail.com>
2019-09-17 12:17:34 +0100

    Added setupLocal script for devs working with a vagrant+diku setup

---
+++ other-scripts/setupLocal.groovy
@@ -0,0 +1,12 @@
+:load ./modrsCli.groovy
+okapi=new OkapiClient('local')
+rsclient = new RSClient(okapi);
+okapi.listTenantSymbols()
+
+okapi.addTenantSymbol('OCLC:ZMU');
+okapi.addTenantSymbol('OCLC:PPU');
+okapi.addTenantSymbol('OCLC:PPPA');
+okapi.addTenantSymbol('OCLC:AVL');
+okapi.addTenantSymbol('RESHARE:LOCALSYMBOL01');
+okapi.addTenantSymbol('RESHARE:KNOWINT');
+okapi.listTenantSymbols()
