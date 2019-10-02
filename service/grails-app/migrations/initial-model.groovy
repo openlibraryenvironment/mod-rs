a115afcb9e4e6dc89228d8fc3dcc53413cc55bc8
Ian Ibbotson <ianibbo@googlemail.com>
2019-10-02 10:42:20 +0100

    Add settings

--- service/grails-app/migrations/initial-model.groovy
+++ service/grails-app/migrations/initial-model.groovy
@@ -1244,4 +1244,20 @@ databaseChangeLog = {
 
     }
 
+    changeSet(author: "ianibbo (manual)", id: "20191002-1038-001") {
+        createTable(tableName: "group_member") {
+            column(name: "st_id", type: "VARCHAR(36)") {
+                constraints(nullable: "false")
+            }
+            column(name: "st_version", type: "BIGINT") {
+                constraints(nullable: "false")
+            }
+            column(name: 'st_section', type: "VARCHAR(255)")
+            column(name: 'st_key', type: "VARCHAR(255)")
+            column(name: 'st_setting_type', type: "VARCHAR(255)")
+            column(name: 'st_vocab', type: "VARCHAR(255)")
+            column(name: 'st_default_value', type: "VARCHAR(255)")
+            column(name: 'st_value", type: "VARCHAR(255)")
+        }
+    }
 }
