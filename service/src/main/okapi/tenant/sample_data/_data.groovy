ce66f52b68ac110a10eb78d8999223d13962a16f
Ian Ibbotson <ianibbo@googlemail.com>
2019-10-04 15:24:17 +0100

    commiting to share with steve

--- service/src/main/okapi/tenant/sample_data/_data.groovy
+++ service/src/main/okapi/tenant/sample_data/_data.groovy
@@ -1,3 +1,17 @@
 import org.olf.rs.HostLMSLocation
 import org.olf.okapi.modules.directory.DirectoryEntry
 import org.olf.okapi.modules.directory.Address
+
+import com.k_int.web.toolkit.settings.AppSetting
+
+
+try {
+  println("Create z3950 server address");
+  AppSetting z3950_address = AppSetting.findByKey('z3950_server_address') ?: new AppSetting(
+   key: 'z3950_server_address'
+  ).save(flush:true, failOnError: true);
+  println("DONE Create z3950 server address");
+}
+catch ( Exception e ) {
+  e.printStackTrace();
+}
