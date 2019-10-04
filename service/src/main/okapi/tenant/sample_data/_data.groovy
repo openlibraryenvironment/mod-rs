a658b04cd5ae01e67992353dd43f8c2dc0353252
Ethan Freestone <ethan.freestone@k-int.co.uk>
2019-10-04 15:58:21 +0100

    git whoopsie

--- service/src/main/okapi/tenant/sample_data/_data.groovy
+++ service/src/main/okapi/tenant/sample_data/_data.groovy
@@ -1,10 +1,9 @@
 import org.olf.rs.HostLMSLocation
 import org.olf.okapi.modules.directory.DirectoryEntry
 import org.olf.okapi.modules.directory.Address
-import com.k_int.web.toolkit.settings.AppSetting
+/* import com.k_int.web.toolkit.settings.AppSetting
 
 AppSetting Z3950 = AppSetting.findByKey('z3950') ?: new AppSetting(
-  version: 0,
   key: 'z3950',
   value: '',
-).save(flush:true, failOnError: true);
+).save(flush:true, failOnError: true); */
