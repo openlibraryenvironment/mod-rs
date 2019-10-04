235c3cae6f4b5bfcfab4cc765c26b666bde01d7c
Ethan Freestone <ethan.freestone@k-int.co.uk>
2019-10-04 10:32:44 +0100

    Added Z3950 setting to bootstrap in backend

--- service/src/main/okapi/tenant/sample_data/_data.groovy
+++ service/src/main/okapi/tenant/sample_data/_data.groovy
@@ -2,4 +2,8 @@ import org.olf.rs.HostLMSLocation
 import org.olf.okapi.modules.directory.DirectoryEntry
 import org.olf.okapi.modules.directory.Address
 
-println("no test data");
+AppSetting Z3950 = AppSetting.findByKey('z3950') ?: new AppSetting(
+  version: 0,
+  key: 'z3950'
+  value: '',
+);.save(flush:true, failOnError: true);
