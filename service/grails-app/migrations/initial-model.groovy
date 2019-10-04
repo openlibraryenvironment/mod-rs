b17916fb593ec30d8d79af1d268d7c0d110fb4c5
Ian Ibbotson <ianibbo@googlemail.com>
2019-10-04 14:51:37 +0100

    setting table should be app_setting - add new migration

--- service/grails-app/migrations/initial-model.groovy
+++ service/grails-app/migrations/initial-model.groovy
@@ -1244,23 +1244,6 @@ databaseChangeLog = {
 
     }
 
-    changeSet(author: "ianibbo (manual)", id: "20191002-1038-001") {
-        createTable(tableName: "setting") {
-            column(name: "st_id", type: "VARCHAR(36)") {
-                constraints(nullable: "false")
-            }
-            column(name: "st_version", type: "BIGINT") {
-                constraints(nullable: "false")
-            }
-            column(name: 'st_section', type: "VARCHAR(255)")
-            column(name: 'st_key', type: "VARCHAR(255)")
-            column(name: 'st_setting_type', type: "VARCHAR(255)")
-            column(name: 'st_vocab', type: "VARCHAR(255)")
-            column(name: 'st_default_value', type: "VARCHAR(255)")
-            column(name: 'st_value', type: "VARCHAR(255)")
-        }
-    }
-
     changeSet(author: "ianibbo (manual)", id: "20191003-0931-001") {
 
         addColumn(tableName: "host_lms_location") {
@@ -1279,4 +1262,22 @@ databaseChangeLog = {
             column(name:'pr_peer_request_identifier', type: "VARCHAR(255)") 
        }
     }
+
+    changeSet(author: "ianibbo (manual)", id: "20191004-1451-001") {
+        createTable(tableName: "app_setting") {
+            column(name: "st_id", type: "VARCHAR(36)") {
+                constraints(nullable: "false")
+            }
+            column(name: "st_version", type: "BIGINT") {
+                constraints(nullable: "false")
+            }
+            column(name: 'st_section', type: "VARCHAR(255)")
+            column(name: 'st_key', type: "VARCHAR(255)")
+            column(name: 'st_setting_type', type: "VARCHAR(255)")
+            column(name: 'st_vocab', type: "VARCHAR(255)")
+            column(name: 'st_default_value', type: "VARCHAR(255)")
+            column(name: 'st_value', type: "VARCHAR(255)")
+        }
+    }
+
 }
