b37fad575d45c1726d35351e963f2d0503cc8d58
Ethan Freestone <ethan.freestone@k-int.co.uk>
2019-10-04 10:41:44 +0100

    imported AppSetting to _data file

--- service/src/main/okapi/tenant/sample_data/_data.groovy
+++ service/src/main/okapi/tenant/sample_data/_data.groovy
@@ -1,6 +1,7 @@
 import org.olf.rs.HostLMSLocation
 import org.olf.okapi.modules.directory.DirectoryEntry
 import org.olf.okapi.modules.directory.Address
+import com.k_int.web.toolkit.settings.AppSetting
 
 AppSetting Z3950 = AppSetting.findByKey('z3950') ?: new AppSetting(
   version: 0,
