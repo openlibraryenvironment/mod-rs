93dac63a3f9eaa153463eb1fab85bb6374c76590
Ian Ibbotson <ianibbo@googlemail.com>
2019-10-11 09:43:14 +0100

    Work on externally received iso 18626 messages

--- okapi-scripts/iso18626.sh
+++ okapi-scripts/iso18626.sh
@@ -13,29 +13,4 @@ fi
 
 
 # curl -s -H "Content-Type: application/xml" --header "X-Okapi-Tenant: ${TENANT}" -H "X-Okapi-Token: ${AUTH_TOKEN}" -X POST http://localhost:9130/rs/iso18626 -d @- <<EOF
-curl -s -H "Content-Type: application/xml" -X POST http://localhost:9130/rs/iso18626 -d @- <<EOF
-<ISO18626Message ill:version="1.0" xsi:schemaLocation="http://illtransactions.org/2013/iso18626 http://illtransactions.org/schemas/ISO-18626-v1_1.xsd">
-  <request>
-    <header>
-      <supplyingAgencyId>
-        <agencyIdType>ISIL</agencyIdType><agencyIdValue>DK-710100</agencyIdValue>
-      </supplyingAgencyId>
-      <requestingAgencyId>
-        <agencyIdType>ISIL</agencyIdType>
-        <agencyIdValue>DK-820010</agencyIdValue>
-      </requestingAgencyId>
-      <timestamp>2014-03-17T09:30:47.0Z</timestamp>
-      <requestingAgencyRequestId>49837539</requestingAgencyRequestId>
-    </header>
-    <bibliographicInfo>
-      <supplierUniqueRecordId>27345786</supplierUniqueRecordId>
-    </bibliographicInfo>
-    <serviceInfo>
-      <serviceType>Loan</serviceType>
-      <serviceLevel>Normal</serviceLevel>
-      <needBeforeDate>2014-05-01T00:00:00.0Z</needBeforeDate>
-      <anyEdition>Y</anyEdition>
-    </serviceInfo>
-  </request>
-</ISO18626Message>
-EOF
+curl -s -H "Content-Type: application/xml" -X POST http://localhost:8081/rs/iso18626 -d @iso18626_req_1.xml
--- service/grails-app/controllers/mod/rs/iso18626Controller.groovy
+++ service/grails-app/controllers/mod/rs/iso18626Controller.groovy
@@ -13,6 +13,13 @@ class iso18626Controller {
   def index() {
     def result=[status:'ok']
     log.debug("iso18626Controller::index(${params})");
+    log.debug("XML: ${request.XML}");
+    render result as JSON
+  }
+
+  def symbol() {
+    def result=[status:'ok']
+    log.debug("iso18626Controller::symbol(${params})");
     render result as JSON
   }
 }
