022a4786018f6a036a13d852f38f4c39cbaf6754
Ian Ibbotson <ianibbo@googlemail.com>
2019-10-11 10:16:30 +0100

    more work on external request processing - extract a target symbol and use the globalConfig to see if that symbol belongs to a tenant

--- service/grails-app/controllers/mod/rs/iso18626Controller.groovy
+++ service/grails-app/controllers/mod/rs/iso18626Controller.groovy
@@ -9,11 +9,51 @@ import org.olf.rs.BackgroundTaskService;
 class iso18626Controller {
 
   GrailsApplication grailsApplication
+  GlobalConfigService globalConfigService
 
   def index() {
     def result=[status:'ok']
     log.debug("iso18626Controller::index(${params})");
-    log.debug("XML: ${request.XML}");
+
+    try {
+      log.debug("XML: ${request.XML}");
+      def iso18626_msg = request.XML;
+      String recipient;
+      String tenant;
+
+      if ( iso18626_msg.request != null ) {
+        log.debug("Process inbound request message");
+        // Look in request.header.supplyingAgencyId for the intended recipient
+        recipient = getSymbolFor(iso18626_msg.request.header.supplyingAgencyId);
+        tenant = globalConfigService.getTenantForSymbol(recipient);
+        if ( tenant ) {
+          log.debug("incoming request for ${tenant}");
+        }
+      }
+      else if ( iso18626_msg.supplyingAgencyMessage != null ) {
+        log.debug("Process inbound supplyingAgencyMessage message");
+        // Look in request.header.requestingAgencyId for the intended recipient
+        recipient = getSymbolFor(iso18626_msg.request.header.requestingAgencyId);
+        tenant = globalConfigService.getTenantForSymbol(recipient);
+        if ( tenant ) {
+          log.debug("incoming request for ${tenant}");
+        }
+      }
+      else if ( iso10626_msg.requestingAgencyMessageConfirmation != null ) {
+        log.debug("Process inbound requestingAgencyMessage message");
+        // Look in request.header.supplyingAgencyId for the intended recipient
+        recipient = getSymbolFor(iso18626_msg.request.header.supplyingAgencyId);
+        tenant = globalConfigService.getTenantForSymbol(recipient);
+        if ( tenant ) {
+          log.debug("incoming request for ${tenant}");
+        }
+      }
+
+    }
+    catch ( Exception e ) {
+      e.printStackTrace()
+    }
+
     render result as JSON
   }
 
@@ -21,5 +61,17 @@ class iso18626Controller {
     def result=[status:'ok']
     log.debug("iso18626Controller::symbol(${params})");
     render result as JSON
+  }
+
+  private String getSymbolFor(path) {
+    String result = null;
+    if ( path.agencyIdType != null && path.agencyIdValue != null ) {
+      result = "${path.agencyIdType.text()}:${path.agencyIdValue.text()}".toString()
+    }
+    else {
+      log.error("Missing agency id type or value");
+    }
+    log.debug("Returning symbol : ${result}");
+    return result;
   }
 }
