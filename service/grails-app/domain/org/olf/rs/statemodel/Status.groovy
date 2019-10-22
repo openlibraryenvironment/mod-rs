84c7f493d3304ab8e49dcdc5115d546c6fb7e29e
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2019-10-22 15:41:04 +0100

    Templates and import tidying.

--- service/grails-app/domain/org/olf/rs/statemodel/Status.groovy
+++ service/grails-app/domain/org/olf/rs/statemodel/Status.groovy
@@ -1,11 +1,6 @@
 package org.olf.rs.statemodel
 
-import grails.gorm.multitenancy.Tenants;
 import grails.gorm.MultiTenant
-import com.k_int.web.toolkit.refdata.RefdataValue
-import com.k_int.web.toolkit.custprops.CustomProperties
-import com.k_int.web.toolkit.refdata.CategoryId
-import com.k_int.web.toolkit.refdata.Defaults
 
 /**
  * PatronRequest - Instances of this class represent an occurrence of a patron (Researcher, Undergrad, Faculty)
