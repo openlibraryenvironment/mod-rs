763d3000d555a344cca413d036dcac0cb21d185d
Ian Ibbotson <ianibbo@googlemail.com>
2019-10-27 21:02:01 +0000

    Added demoCreate

---
+++ other-scripts/demoCreate.groovy
@@ -0,0 +1,18 @@
+:load ./modrsCli.groovy
+okapi=new OkapiClient('reshare')
+rsclient = new RSClient(okapi);
+okapi.listTenantSymbols()
+
+// okapi.createRequest([
+//                      title:'The Heart of Enterprise',
+//                      patronIdentifier: 'PI',
+//                      patronReference: 'PR',
+//                      patronSurname: 'PS',
+//                      patronGivenName: 'PGN',
+//                      patronType:'PT',
+//                      requestingInstitutionSymbol:'RESHARE:KNOWINT']);
+
+//okapi.createRequest([title:'The Heart of Enterprise', requestingInstitutionSymbol:'OCLC:AVL']);
+okapi.createRequest([title:'Temeraire', requestingInstitutionSymbol:'OCLC:ZMU']);
+
+
