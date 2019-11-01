621b673e71438b55410e72c0646de525bc458bc9
Ian Ibbotson <ianibbo@googlemail.com>
2019-11-01 15:19:17 +0000

    added some extra settings definitions

--- service/src/main/okapi/tenant/sample_data/_data.groovy
+++ service/src/main/okapi/tenant/sample_data/_data.groovy
@@ -6,10 +6,24 @@ import com.k_int.web.toolkit.settings.AppSetting
 
 try {
   println("Create z3950 server address");
-  AppSetting z3950_address = AppSetting.findByKey('z3950_server_address') ?: new AppSetting(
-   key: 'z3950_server_address'
-  ).save(flush:true, failOnError: true);
-  println("DONE Create z3950 server address");
+  AppSetting z3950_address = AppSetting.findByKey('z3950_server_address') ?: new AppSetting( 
+                                  section:'z3950',
+                                  settingType:'String',
+                                  key: 'z3950_server_address',
+                                  ).save(flush:true, failOnError: true);
+
+  AppSetting ncip_address = AppSetting.findByKey('ncip_server_address') ?: new AppSetting( 
+                                  section:'Local NCIP',
+                                  settingType:'String',
+                                  key: 'ncip_server_address'
+                                  ).save(flush:true, failOnError: true);
+
+  AppSetting borrower_check = AppSetting.findByKey('borrower_check') ?: new AppSetting( 
+                                  section:'Requester Validation',
+                                  settingType:'Refdata',
+                                  vocab:'BorrowerCheckMethod',
+                                  key: 'borrower_check'
+                                  ).save(flush:true, failOnError: true);
 }
 catch ( Exception e ) {
   e.printStackTrace();
