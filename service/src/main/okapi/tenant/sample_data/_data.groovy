3f6139503d58992f796e6c10c87e51126e25ef05
Ian Ibbotson <ianibbo@googlemail.com>
2019-11-11 09:22:30 +0000

    Add request_id_prefix

--- service/src/main/okapi/tenant/sample_data/_data.groovy
+++ service/src/main/okapi/tenant/sample_data/_data.groovy
@@ -2,6 +2,7 @@ import org.olf.rs.HostLMSLocation
 import org.olf.okapi.modules.directory.DirectoryEntry
 import org.olf.okapi.modules.directory.Address
 import com.k_int.web.toolkit.settings.AppSetting
+import com.k_int.web.toolkit.refdata.*
 
 
 try {
@@ -18,12 +19,22 @@ try {
                                   key: 'ncip_server_address'
                                   ).save(flush:true, failOnError: true);
 
+  RefdataValue.lookupOrCreate('BorrowerCheckMethod', 'None');
+  RefdataValue.lookupOrCreate('BorrowerCheckMethod', 'NCIP2');
+
   AppSetting borrower_check = AppSetting.findByKey('borrower_check') ?: new AppSetting( 
                                   section:'Requester Validation',
                                   settingType:'Refdata',
                                   vocab:'BorrowerCheckMethod',
                                   key: 'borrower_check'
                                   ).save(flush:true, failOnError: true);
+
+  AppSetting z3950_address = AppSetting.findByKey('request_id_prefix') ?: new AppSetting( 
+                                  section:'requests',
+                                  settingType:'String',
+                                  key: 'request_id_prefix',
+                                  ).save(flush:true, failOnError: true);
+
 }
 catch ( Exception e ) {
   e.printStackTrace();
