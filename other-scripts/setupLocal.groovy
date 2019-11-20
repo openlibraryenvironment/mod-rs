e436c9b2433269fa205897e799bb03c0692016bc
Ian Ibbotson <ianibbo@googlemail.com>
2019-11-20 17:01:52 +0000

    added more test records

--- other-scripts/setupLocal.groovy
+++ other-scripts/setupLocal.groovy
@@ -44,6 +44,15 @@ if (initial_setup) {
 
 if (initial_setup) {
   okapi.createRequest([title:'The Heart of Enterprise',requestingInstitutionSymbol:'RESHARE:KNOWINT01']);
+  okapi.createRequest([
+                       title:'The darkening land',
+                       patronIdentifier: 'PI',
+                       patronReference: 'PR',
+                       patronSurname: 'PS',
+                       patronGivenName: 'PGN',
+                       systemInstanceIdentifier:'3246e0db-6d41-442b-ae61-27f1d607a8dc', 
+                       requestingInstitutionSymbol:'RESHARE:KNOWINT01']);
+
 }
 
 if ( !initial_setup) {
@@ -57,6 +66,18 @@ if ( !initial_setup) {
   //                      systemInstanceIdentifier:'8a6d65a3-709c-4ade-9ffa-043fb031fedd',
   //                      requestingInstitutionSymbol:'OCLC:ZMU']);
 
+  // 3246e0db-6d41-442b-ae61-27f1d607a8dc | The darkening land
+  // 593bca64-d19e-4d0d-95e0-219d0c36a3d1 | Medieval households
+  // ecd5e043-c3a8-4eab-a9ae-7b9453d1bd57 | Astonishment and power
+  // 3ce768c6-a3ef-40ac-8c69-d98aed2f50a5 | Poetry and prayer
+  // 8c34033e-715d-4d76-8576-0bebd21b174c | English romantic poets
+  // 4e6441a9-5d77-4cb5-a244-3d5a6d65d0ca | Etruscan Italy
+  // c002cadb-6d3d-4bcc-8d29-6d76d0848e9f | The falcons of the world
+  // be137ebc-c2de-4677-8c0e-f5867041b54b | Drug dependence
+  // dd877111-0e99-41c3-999e-e336b96ada80 | Four poets on poetry
+  // cb173644-b00a-46ec-93db-eaba47896ed1 | The Oxford dictionary of Popes
+
+
   okapi.createRequest([
                        title:'10,000 Teachers, 10 Million Minds Science and Math Scholarship Act : report (to accompany H.R. 362) (including cost estimate of the Congressional Budget Office)', 
                        patronIdentifier: 'PI',
@@ -65,6 +86,16 @@ if ( !initial_setup) {
                        patronGivenName: 'PGN',
                        patronType:'PT',
                        systemInstanceIdentifier:'491fe34f-ea1b-4338-ad20-30b8065a7b46',
+                       requestingInstitutionSymbol:'OCLC:ZMU']);
+
+  okapi.createRequest([
+                       title:'Medieval households',
+                       patronIdentifier: 'PI',
+                       patronReference: 'PR',
+                       patronSurname: 'PS',
+                       patronGivenName: 'PGN',
+                       patronType:'PT',
+                       systemInstanceIdentifier:'593bca64-d19e-4d0d-95e0-219d0c36a3d1',
                        requestingInstitutionSymbol:'OCLC:ZMU']);
 }
 
