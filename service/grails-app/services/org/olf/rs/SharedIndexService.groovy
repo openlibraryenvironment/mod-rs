d3c5158483956aac16216bbcc66ac3932113d225
Ian Ibbotson <ianibbo@gmail.com>
2019-12-03 13:50:26 +0000

    More error trapping

--- service/grails-app/services/org/olf/rs/SharedIndexService.groovy
+++ service/grails-app/services/org/olf/rs/SharedIndexService.groovy
@@ -140,29 +140,34 @@ public class SharedIndexService {
     String result = null;
     def postBody = [username: user, password: pass]
     log.debug("getOkapiToken(${baseUrl},${postBody},..,${tenant})");
-    def r1 = configure {
-      request.headers['X-Okapi-Tenant'] = tenant
-      request.headers['accept'] = 'application/json'
-      request.contentType = 'application/json'
-      request.uri = baseUrl+'/bl-users/login'
-      request.uri.query = [expandPermissions:true,fullPermissions:true]
-      request.body = postBody
-    }.get() {
-      response.success { resp ->
-        if ( resp == null ) {
-          log.error("Response null from http post");
-        }
-        else {
-          log.debug("Try to extract token - ${resp} ${resp?.headers}");
-          def tok_header = resp.headers?.find { h-> h.key == 'x-okapi-token' }
-          if ( tok_header ) {
-            result = tok_header.value;
+    try {
+      def r1 = configure {
+        request.headers['X-Okapi-Tenant'] = tenant
+        request.headers['accept'] = 'application/json'
+        request.contentType = 'application/json'
+        request.uri = baseUrl+'/bl-users/login'
+        request.uri.query = [expandPermissions:true,fullPermissions:true]
+        request.body = postBody
+      }.get() {
+        response.success { resp ->
+          if ( resp == null ) {
+            log.error("Response null from http post");
           }
           else {
-            log.warn("Unable to locate okapi token header amongst ${r1?.headers}");
+            log.debug("Try to extract token - ${resp} ${resp?.headers}");
+            def tok_header = resp.headers?.find { h-> h.key == 'x-okapi-token' }
+            if ( tok_header ) {
+              result = tok_header.value;
+            }
+            else {
+              log.warn("Unable to locate okapi token header amongst ${r1?.headers}");
+            }
           }
-        }
         
+        }
+      }
+      catch ( Exception e ) {
+        log.error("problem trying to obtain auth token for shared index",e);
       }
     }
 
