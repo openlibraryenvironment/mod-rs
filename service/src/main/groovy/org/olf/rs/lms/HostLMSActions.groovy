45bc8c77c9f54053020bedd5d8dbb5385142a0c0
Ian Ibbotson <ianibbo@googlemail.com>
2020-01-08 11:40:23 +0000

    added some javadoc

--- service/src/main/groovy/org/olf/rs/lms/HostLMSActions.groovy
+++ service/src/main/groovy/org/olf/rs/lms/HostLMSActions.groovy
@@ -4,8 +4,13 @@ import org.olf.rs.PatronRequest;
 
 public interface HostLMSActions {
 
-       public boolean checkoutItem(String itemBarcode, String borrowerBarcode);
-           public Map lookupPatron(String patron_id);
+  public boolean checkoutItem(String itemBarcode, String borrowerBarcode);
+
+  /**
+   * @return a Map containing the following keys userid, givenName, surname, status
+   */
+  public Map lookupPatron(String patron_id);
+
   public ItemLocation determineBestLocation(PatronRequest pr);
 
 }
