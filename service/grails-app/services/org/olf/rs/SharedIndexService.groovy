136cf38ab6d564b0729d9a774d8b035b922f006f
Ian Ibbotson <ianibbo@googlemail.com>
2020-01-10 13:31:57 +0000

    Add setting for shared index tenant

--- service/grails-app/services/org/olf/rs/SharedIndexService.groovy
+++ service/grails-app/services/org/olf/rs/SharedIndexService.groovy
@@ -64,11 +64,12 @@ public class SharedIndexService {
     AppSetting shared_index_base_url_setting = AppSetting.findByKey('shared_index_base_url');
     AppSetting shared_index_user_setting = AppSetting.findByKey('shared_index_user');
     AppSetting shared_index_pass_setting = AppSetting.findByKey('shared_index_pass');
+    AppSetting shared_index_tenant_setting = AppSetting.findByKey('shared_index_tenant');
 
     String shared_index_base_url = shared_index_base_url_setting?.value ?: shared_index_base_url_setting?.defValue;
     String shared_index_user = shared_index_user_setting?.value ?: shared_index_user_setting?.defValue;
     String shared_index_pass = shared_index_pass_setting?.value ?: shared_index_pass_setting?.defValue;
-    String shared_index_tenant = 'diku'
+    String shared_index_tenant =  shared_index_tenant_setting?.value ?: shared_index_tenant_setting?.defValue ?: 'diku'
 
     if ( ( shared_index_base_url != null ) &&
          ( shared_index_user != null ) &&
@@ -164,11 +165,12 @@ public class SharedIndexService {
     AppSetting shared_index_base_url_setting = AppSetting.findByKey('shared_index_base_url');
     AppSetting shared_index_user_setting = AppSetting.findByKey('shared_index_user');
     AppSetting shared_index_pass_setting = AppSetting.findByKey('shared_index_pass');
+    AppSetting shared_index_tenant_setting = AppSetting.findByKey('shared_index_tenant');
 
     String shared_index_base_url = shared_index_base_url_setting?.value ?: shared_index_base_url_setting?.defValue;
     String shared_index_user = shared_index_user_setting?.value ?: shared_index_user_setting?.defValue;
     String shared_index_pass = shared_index_pass_setting?.value ?: shared_index_pass_setting?.defValue;
-    String shared_index_tenant = 'diku'
+    String shared_index_tenant =  shared_index_tenant_setting?.value ?: shared_index_tenant_setting?.defValue ?: 'diku'
 
     if ( ( shared_index_base_url != null ) &&
          ( shared_index_user != null ) &&
