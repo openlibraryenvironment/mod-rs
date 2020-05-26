1d00ad37186bf1cb35627a70d5519b10cab9bbb6
Jason Skomorowski <jason@indexdata.com>
2020-05-26 15:53:33 -0400

    RES_ERROR also needs attention

--- service/grails-app/services/org/olf/rs/HousekeepingService.groovy
+++ service/grails-app/services/org/olf/rs/HousekeepingService.groovy
@@ -115,7 +115,7 @@ public class HousekeepingService {
         Status.lookupOrCreate('Responder', 'RES_COMPLETE', '0040', true);
         Status.lookupOrCreate('Responder', 'RES_CANCEL_REQUEST_RECEIVED', '9998', true);
         Status.lookupOrCreate('Responder', 'RES_CANCELLED', '9999', true);
-        Status.lookupOrCreate('Responder', 'RES_ERROR', '9999', true);
+        Status.lookupOrCreate('Responder', 'RES_ERROR', '9999', true, true);
 
         AvailableAction.ensure( 'Responder', 'RES_AWAIT_LMS_CHECKOUT', 'supplierManualCheckout', 'M')
         AvailableAction.ensure( 'Responder', 'RES_AWAIT_LMS_CHECKOUT', 'message', 'M')
