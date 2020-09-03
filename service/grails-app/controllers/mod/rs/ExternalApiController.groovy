ac20988a4db32ff00c8de631d00d05e2bc2e4a5a
EthanFreestone <ethan.freestone@k-int.co.uk>
2020-09-03 11:49:31 +0100

    split some methods out to a separate service, removed delegate call

--- service/grails-app/controllers/mod/rs/ExternalApiController.groovy
+++ service/grails-app/controllers/mod/rs/ExternalApiController.groovy
@@ -8,6 +8,7 @@ import grails.converters.JSON
 import org.olf.rs.GlobalConfigService
 import org.olf.rs.BackgroundTaskService;
 import org.olf.rs.ReshareApplicationEventHandlerService
+import org.olf.rs.ConfirmationMessageService
 import grails.gorm.multitenancy.Tenants
 import java.text.SimpleDateFormat
 import groovy.xml.StreamingMarkupBuilder
@@ -26,6 +27,7 @@ class ExternalApiController {
   GrailsApplication grailsApplication
   GlobalConfigService globalConfigService
   ReshareApplicationEventHandlerService reshareApplicationEventHandlerService
+  ConfirmationMessageService confirmationMessageService
 
   def index() {
   }
@@ -51,26 +53,29 @@ class ExternalApiController {
 
   def iso18626() {
     def result=[status:'ok']
-    log.debug("iso18626Controller::index(${params})");
+    log.debug("externalApiController::index(${params})");
 
     try {
       if ( request.XML != null ) {
         org.grails.databinding.xml.GPathResultMap iso18626_msg = new org.grails.databinding.xml.GPathResultMap(request.XML);
         log.debug("GPATH MESSAGE: ${iso18626_msg}")
+
+        // TODO This string is purely used for logging messages atm--decide if necessary
         String recipient;
-  
+
         if ( iso18626_msg.request != null ) {
           log.debug("Process inbound request message");
   
           def mr = iso18626_msg.request;
-          def req_result = makeDefaultReqResult(mr, "REQUEST");
-  
+
           // Look in request.header.supplyingAgencyId for the intended recipient
-          recipient = getSymbolFor(mr.header.supplyingAgencyId);
+          recipient = mr.header.supplyingAgencyId
+          log.debug("incoming request message for ${recipient} ${getSymbolFor(recipient)}");
+
           req_result = reshareApplicationEventHandlerService.handleRequestMessage(mr);
           log.debug("result of req_request ${req_result}");
   
-          def confirmationMessage = makeConfirmationMessage(delegate, req_result)
+          def confirmationMessage = confirmationMessageService.makeConfirmationMessage(delegate, req_result)
           StringWriter sw = new StringWriter();
           sw << new StreamingMarkupBuilder().bind (confirmationMessage)
           String message = sw.toString();
@@ -85,17 +90,15 @@ class ExternalApiController {
           log.debug("Process inbound supplyingAgencyMessage message");
   
           def msam = iso18626_msg.supplyingAgencyMessage;
-          def req_result = makeDefaultReqResult(msam, "SUPPLYING_AGENCY_MESSAGE");
   
           // Look in request.header.requestingAgencyId for the intended recipient
-          recipient = getSymbolFor(msam.header.requestingAgencyId);
-          log.debug("incoming supplying agency message for ${msam.header.requestingAgencyId} ${recipient}");
+          recipient = msam.header.requestingAgencyId
+          log.debug("incoming supplying agency message for ${recipient} ${getSymbolFor(recipient)}");
   
           req_result = reshareApplicationEventHandlerService.handleSupplyingAgencyMessage(msam);
-  
           log.debug("result of req_request ${req_result}");
   
-          def confirmationMessage = makeConfirmationMessage(delegate, req_result)
+          def confirmationMessage = confirmationMessageService.makeConfirmationMessage(delegate, req_result)
           StringWriter sw = new StringWriter();
           sw << new StreamingMarkupBuilder().bind (confirmationMessage)
           String message = sw.toString();
@@ -110,16 +113,15 @@ class ExternalApiController {
           log.debug("Process inbound requestingAgencyMessage message");
   
           def mram = iso18626_msg.requestingAgencyMessage;
-          def req_result = makeDefaultReqResult(mram, "REQUESTING_AGENCY_MESSAGE");
-  
+
           // Look in request.header.supplyingAgencyId for the intended recipient
-          recipient = getSymbolFor(mram.header.supplyingAgencyId);
-          log.debug("incoming requesting agency message for ${mram.header.supplyingAgencyId} ${recipient}");
+          recipient = mram.header.supplyingAgencyId;
+          log.debug("incoming requesting agency message for ${recipient} ${getSymbolFor(recipient)}");
 
           req_result = reshareApplicationEventHandlerService.handleRequestingAgencyMessage(mram);
           log.debug("result of req_request ${req_result}");
   
-          def confirmationMessage = makeConfirmationMessage(delegate, req_result)
+          def confirmationMessage = confirmationMessageService.makeConfirmationMessage(delegate, req_result)
           StringWriter sw = new StringWriter();
           sw << new StreamingMarkupBuilder().bind (confirmationMessage)
           String message = sw.toString();
@@ -147,7 +149,7 @@ class ExternalApiController {
 
   def symbol() {
     def result=[status:'ok']
-    log.debug("iso18626Controller::symbol(${params})");
+    log.debug("externalApiController::symbol(${params})");
     render result as JSON
   }
 
@@ -161,104 +163,5 @@ class ExternalApiController {
     }
     log.debug("Returning symbol : ${result}");
     return result;
-  }
-
-
-  void exec ( def del, Closure c ) {
-    c.rehydrate(del, c.owner, c.thisObject)()
-  } 
-
-
-  def makeDefaultReqResult(incomingRequest, messageType) {
-    def req_result = [:];
-    if (messageType ==! null) {
-      throw new Exception("makeDefaultReqResult expected a messageType");
-      return req_result;
-    } else {
-      req_result.messageType = messageType
-      req_result.supIdType = incomingRequest.header.supplyingAgencyId.agencyIdType
-      req_result.supId = incomingRequest.header.supplyingAgencyId.agencyIdValue
-      req_result.reqAgencyIdType = incomingRequest.header.requestingAgencyId.agencyIdType
-      req_result.reqAgencyId = incomingRequest.header.requestingAgencyId.agencyIdValue
-      req_result.reqId = incomingRequest.header.requestingAgencyRequestId
-      req_result.timeRec = incomingRequest.header.timestamp
-    }
-    if (messageType == "SUPPLYING_AGENCY_MESSAGE") {
-      req_result.reasonForMessage = incomingRequest?.messageInfo?.reasonForMessage
-    }
-    if (messageType == "REQUESTING_AGENCY_MESSAGE") {
-      req_result.action = incomingRequest?.activeSection?.action
-    }
-    
-    return req_result;
-  }
-
-  // This method creates the confirmation message
-  def makeConfirmationMessage(def del, def req_result) {
-    SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-    def currentTime = dateFormatter.format(new Date())
-    return {
-      exec(del) {
-        ISO18626Message( 'ill:version':'1.0',
-                        'xmlns':'http://illtransactions.org/2013/iso18626',
-                        'xmlns:ill': 'http://illtransactions.org/2013/iso18626',
-                        'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
-                        'xsi:schemaLocation': 'http://illtransactions.org/2013/iso18626 http://illtransactions.org/schemas/ISO-18626-v1_1.xsd' ) {
-          
-          switch (req_result.messageType) {
-            case "REQUEST":
-              requestConfirmation {
-                makeConfirmationMessageBody(delegate, req_result);
-              }
-              break;
-            case "SUPPLYING_AGENCY_MESSAGE":
-              supplyingAgencyMessageConfirmation {
-                makeConfirmationMessageBody(delegate, req_result);
-              }
-              break;
-            case "REQUESTING_AGENCY_MESSAGE":
-              requestingAgencyMessageConfirmation {
-                makeConfirmationMessageBody(delegate, req_result);
-              }
-              break;
-            default:
-              throw new Exception ("makeConfirmationMessage expects passed req_result to contain a valid messageType")
-          }
-        }
-      }
-    }
-  }
-
-  void makeConfirmationMessageBody(def del, def req_result) {
-    SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-    def currentTime = dateFormatter.format(new Date())
-    exec(del) {
-      confirmationHeader {
-        supplyingAgencyId {
-          agencyIdType(req_result.supIdType)
-          agencyIdValue(req_result.supId)
-        }
-        requestingAgencyId {
-          agencyIdType(req_result.reqAgencyIdType)
-          agencyIdValue(req_result.reqAgencyId)
-        }
-        timestamp(currentTime)
-        requestingAgencyRequestId(req_result.reqId)
-        timestampReceived(req_result.timeRec)
-        messageStatus(req_result.status)
-        if (req_result.status != "OK") {
-          errorData {
-            errorType(req_result.errorType)
-            errorValue(req_result.errorValue)
-          }
-        }
-        if (req_result.messageType == "SUPPLYING_AGENCY_MESSAGE") {
-          reasonForMessage(req_result.reasonForMessage)
-        }
-        if (req_result.messageType == "REQUESTING_AGENCY_MESSAGE") {
-          action(req_result.action)
-        }
-      }
-    }
   }
 }
---
+++ service/grails-app/services/org/olf/rs/ConfirmationMessageService.groovy
@@ -0,0 +1,85 @@
+package org.olf.rs;
+
+import groovy.lang.Closure
+import org.olf.rs.PatronRequest
+import org.olf.okapi.modules.directory.Symbol;
+
+
+class ConfirmationMessageService {
+
+  // This method creates a confirmation message
+  public def makeConfirmationMessage(def req_result) {
+    SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+    def currentTime = dateFormatter.format(new Date())
+    return {
+        ISO18626Message( 'ill:version':'1.0',
+                        'xmlns':'http://illtransactions.org/2013/iso18626',
+                        'xmlns:ill': 'http://illtransactions.org/2013/iso18626',
+                        'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
+                        'xsi:schemaLocation': 'http://illtransactions.org/2013/iso18626 http://illtransactions.org/schemas/ISO-18626-v1_1.xsd' ) {
+          
+          switch (req_result.messageType) {
+            // The main difference is which heading the confirmation body comes under
+            case "REQUEST":
+              requestConfirmation {
+                makeConfirmationMessageBody(delegate, req_result);
+              }
+              break;
+            case "SUPPLYING_AGENCY_MESSAGE":
+              supplyingAgencyMessageConfirmation {
+                makeConfirmationMessageBody(delegate, req_result);
+              }
+              break;
+            case "REQUESTING_AGENCY_MESSAGE":
+              requestingAgencyMessageConfirmation {
+                makeConfirmationMessageBody(delegate, req_result);
+              }
+              break;
+            default:
+              throw new Exception ("makeConfirmationMessage expects passed req_result to contain a valid messageType")
+          }
+        }
+    }
+  }
+
+  // This method creates the body of the confirmation message--mostly uniform between the confirmation message types
+  void makeConfirmationMessageBody(def del, def req_result) {
+    SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+    def currentTime = dateFormatter.format(new Date())
+    exec(del) {
+      confirmationHeader {
+        supplyingAgencyId {
+          agencyIdType(req_result.supIdType)
+          agencyIdValue(req_result.supId)
+        }
+        requestingAgencyId {
+          agencyIdType(req_result.reqAgencyIdType)
+          agencyIdValue(req_result.reqAgencyId)
+        }
+        timestamp(currentTime)
+        requestingAgencyRequestId(req_result.reqId)
+        timestampReceived(req_result.timeRec)
+        messageStatus(req_result.status)
+        if (req_result.status != "OK") {
+          errorData {
+            errorType(req_result.errorType)
+            errorValue(req_result.errorValue)
+          }
+        }
+        if (req_result.messageType == "SUPPLYING_AGENCY_MESSAGE") {
+          reasonForMessage(req_result.reasonForMessage)
+        }
+        if (req_result.messageType == "REQUESTING_AGENCY_MESSAGE") {
+          action(req_result.action)
+        }
+      }
+    }
+  }
+
+  // Clever bit of wizardry to allow us to inject the calling class into the builder
+  void exec ( def del, Closure c ) {
+    c.rehydrate(del, c.owner, c.thisObject)()
+  } 
+}
+
+  
