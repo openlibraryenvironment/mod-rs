cf4f6adf9363c292089fd9fe5944c832a1e48e97
Ian Ibbotson <ianibbo@gmail.com>
2020-11-10 13:44:03 +0000

    use trace level debugging on OKAPI client to try to find root cause of socket exception on mod-email call in test env

--- service/src/main/groovy/org/olf/rs/FolioEmailServiceImpl.groovy
+++ service/src/main/groovy/org/olf/rs/FolioEmailServiceImpl.groovy
@@ -31,7 +31,7 @@ public class FolioEmailServiceImpl implements EmailService {
 
       if ( okapiClient ) {
         if (okapiClient.withTenant().providesInterface("email", "^1.0")) {
-          log.debug(" -> Got email");
+          log.debug(" -> Got email - calling post ${email_params}");
   
           // post(URL, JsonPayload, Params)
           def email_result = okapiClient.post("/email", email_params, [:]);
@@ -44,7 +44,7 @@ public class FolioEmailServiceImpl implements EmailService {
       }
     }
     catch ( Exception e ) {
-      log.error("Problem talking to mod-config",e);
+      log.error("Problem talking to mod-email",e);
       log.debug("okapiClient: ${okapiClient} ${okapiClient?.inspect()}");
     }
 
