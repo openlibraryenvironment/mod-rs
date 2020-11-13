c7639aaa4d06ac00b2214d8b66ce3d03f10609c8
Ethan Freestone <ethan.freestone@k-int.co.uk>
2020-11-13 11:17:37 +0000

    initial connector code implemented. Request works but don't have access to all the necessary details

--- service/grails-app/services/org/olf/rs/WmsHostLMSService.groovy
+++ service/grails-app/services/org/olf/rs/WmsHostLMSService.groovy
@@ -1,5 +1,9 @@
 package org.olf.rs;
 
+import org.olf.rs.PatronRequest
+import groovyx.net.http.HttpBuilder
+import org.olf.rs.lms.ItemLocation
+
 import org.olf.rs.circ.client.NCIPClientWrapper
 import org.olf.rs.circ.client.CirculationClient
 
@@ -26,4 +30,52 @@ public class WmsHostLMSService extends BaseHostLMSService {
       ]).circulationClient;
   }
 
+  ItemLocation determineBestLocation(PatronRequest pr) {
+    ItemLocation result = null;
+
+    //Override this method on BaseHost to use RTAC connector provided by ID
+    String z3950Connector = "http://mkc.indexdata.com:9008/STU"
+    def reqItem1 = 710733
+    def reqItem2 = 42354631
+
+    log.debug "RESOLVED SYMBOL: ${pr.resolvedSupplier.toString()}"
+
+    def z_response = HttpBuilder.configure {
+        request.uri = z3950Connector
+      }.get {
+          request.uri.query = [
+                                'version':'1.1',
+                                'operation': 'searchRetrieve',
+                                'x-username': 'reshare',
+                                'x-password': 'reshare0618',
+                                'query': "rec.identifier=${reqItem1}&startRecord=1&maximumRecords=1"
+                              ]
+      }
+      log.debug("Got Z3950 response: ${z_response}")
+
+      if ( z_response?.numberOfRecords == 1 ) {
+        // SEE http://mkc.indexdata.com:9008/STU?version=1.1&operation=searchRetrieve&x-username=reshare&x-password=reshare0618&query=rec.identifier=42354631&startRecord=1&maximumRecords=1
+
+        // Got exactly 1 record
+        Map availability_summary = [:]
+        z_response?.records?.record?.recordData?.opacRecord?.holdings?.holding?.each { hld ->
+          log.debug("HOLDING ${hld}");
+          log.debug("HOLDING AVAILABLE NOW ${hld.circulations?.circulation?.availableNow}");
+          log.debug("HOLDING AVAILABLE NOW VALUE ${hld.circulations?.circulation?.availableNow?.@value}");
+          if ( hld.circulations?.circulation?.availableNow?.@value=='1' ) {
+            log.debug("Available now");
+            ItemLocation il = new ItemLocation( location: hld.localLocation, shelvingLocation:hld.shelvingLocation, callNumber:hld.callNumber )
+  
+            if ( result == null ) 
+              result = il;
+  
+            availability_summary[hld.localLocation] = il;
+          }
+        }
+  
+        log.debug("At end, availability summary: ${availability_summary}");
+      }
+      log.debug "LOGDEBUG RESULT: ${result}"
+      return result
+  }
 }
