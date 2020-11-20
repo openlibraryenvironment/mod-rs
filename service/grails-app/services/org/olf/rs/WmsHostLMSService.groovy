ee2afb9892c966aa98a9ca096eb878f85bff7aac
Ethan Freestone <ethan.freestone@k-int.co.uk>
2020-11-20 15:48:01 +0000

    bootstrapped data changes

--- service/grails-app/services/org/olf/rs/WmsHostLMSService.groovy
+++ service/grails-app/services/org/olf/rs/WmsHostLMSService.groovy
@@ -13,15 +13,15 @@ import com.k_int.web.toolkit.settings.AppSetting
 public class WmsHostLMSService extends BaseHostLMSService {
 
   public CirculationClient getCirculationClient(String address) {
-    AppSetting ncip_api_key = AppSetting.findByKey('ncip_api_key')
-    AppSetting ncip_api_secret = AppSetting.findByKey('ncip_api_secret')
+    AppSetting wms_api_key = AppSetting.findByKey('wms_api_key')
+    AppSetting wms_api_secret = AppSetting.findByKey('wms_api_secret')
     AppSetting wms_lookup_patron_endpoint = AppSetting.findByKey('wms_lookup_patron_endpoint')
     
     // TODO this wrapper contains the 'send' command we need and returns a Map rather than JSONObject, consider switching to that instead
     return new NCIPClientWrapper(address, [
       protocol: "WMS",
-      apiKey: ncip_api_key.value,
-      apiSecret: ncip_api_secret.value,
+      apiKey: wms_api_key.value,
+      apiSecret: wms_api_secret.value,
       lookupPatronEndpoint: wms_lookup_patron_endpoint.value
       ]).circulationClient;
   }
