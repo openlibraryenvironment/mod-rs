7ba4d4cbe23d2a6cd3904a6b669bf373b0d205a0
Jason Skomorowski <jason@indexdata.com>
2021-02-04 18:35:22 -0500

    Beans have lowercase initials

--- service/grails-app/services/org/olf/rs/SharedIndexService.groovy
+++ service/grails-app/services/org/olf/rs/SharedIndexService.groovy
@@ -11,13 +11,10 @@ import grails.core.GrailsApplication;
 public class SharedIndexService {
 
   GrailsApplication grailsApplication
-  FolioSharedIndexService folioSharedIndexService
 
   public SharedIndexActions getSharedIndexActionsFor(String si) {
     log.debug("SharedIndexService::getSharedIndexActionsFor(${si})");
-    // SharedIndexActions result = grailsApplication.mainContext."${si}SharedIndexService"
-    // SharedIndexActions result = grailsApplication.mainContext."FolioSharedIndexService"
-    SharedIndexActions result = folioSharedIndexService;
+    SharedIndexActions result = grailsApplication.mainContext."${si}SharedIndexService"
 
     if ( result == null && si != 'none' ) {
       log.warn("Unable to locate SharedIndexActions for ${si}. Did you fail to configure the app_setting \"host_si_integration\". Current options are folio|none");
@@ -28,9 +25,8 @@ public class SharedIndexService {
 
   public SharedIndexActions getSharedIndexActions() {
     SharedIndexActions result = null;
-    // AppSetting host_si_intergation_setting = AppSetting.findByKey('host_si_integration');
-    // String v = host_si_intergation_setting?.value;
-    String v = "Folio";
+    AppSetting host_si_intergation_setting = AppSetting.findByKey('host_si_integration');
+    String v = host_si_intergation_setting?.value ?: 'folio';
     log.debug("Return host si integrations for : ${v} - query application context for bean named ${v}SharedIndexService");
     result = getSharedIndexActionsFor(v);
     return result;
