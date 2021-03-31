8a663ed4cac0dedf582be477697baeb3dcbe6f28
Ian Ibbotson <ianibbo@googlemail.com>
2021-03-31 12:27:42 +0100

    Create request router and ranked supplier interfaces

---
+++ service/src/main/groovy/org/olf/rs/routing/RankedSupplier.groovy
@@ -0,0 +1,14 @@
+package org.olf.rs.routing;
+
+
+/**
+ * a POJO representing a possible supplier with some reason for the selection and
+ * an indication of priority/rank
+ */
+public class RankedSupplier {
+  public String supplier_symbol
+  public String copy_identifier
+  public String ill_policy
+  public Long rank
+  public String rankReason
+}
