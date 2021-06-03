cb106fc0478c2d4eab65d597b083c461a9ec4986
Ethan Freestone <ethan.freestone@k-int.co.uk>
2021-06-03 16:17:32 +0100

    moved sorting to grails render view for patron request volumes

--- service/grails-app/domain/org/olf/rs/PatronRequest.groovy
+++ service/grails-app/domain/org/olf/rs/PatronRequest.groovy
@@ -408,7 +408,7 @@ class PatronRequest implements CustomProperties, MultiTenant<PatronRequest> {
 
     audit(sort:'dateCreated', order:'desc')
 
-    volumes cascade: 'all-delete-orphan', sort: 'name'
+    volumes cascade: 'all-delete-orphan'
   }
 
   /**
