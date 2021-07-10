39de8dcf40be2d632c732a330c40abc68107ccfb
Ian Ibbotson <ianibbo@gmail.com>
2021-07-10 14:17:53 +0100

    Backend work to allow ranking supplying locations

--- service/src/main/groovy/org/olf/rs/lms/ItemLocation.groovy
+++ service/src/main/groovy/org/olf/rs/lms/ItemLocation.groovy
@@ -5,9 +5,9 @@ public class ItemLocation {
   public String location
   public String shelvingLocation
   public String callNumber
-
+  public Long preference
 
   public String toString() {
-    return "ItemLocation(${location},${shelvingLocation},${callNumber})".toString()
+    return "ItemLocation(${location},${shelvingLocation},${callNumber},${preference})".toString()
   }
 }
