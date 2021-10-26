6a6f8cda6396faaa4ee206c5b5793e3a4311030c
Jason Skomorowski <jason@indexdata.com>
2021-10-26 11:51:15 -0400

    Allow WmsHostLMSService to return multiple holdings PR-1119
    Only three through for consistency with the current implementation of
    the base class

--- service/grails-app/services/org/olf/rs/hostlms/WmsHostLMSService.groovy
+++ service/grails-app/services/org/olf/rs/hostlms/WmsHostLMSService.groovy
@@ -36,28 +36,29 @@ public class WmsHostLMSService extends BaseHostLMSService {
       [
         name:'Adapter_By_OCLC_Number',
         precondition: { pr -> return ( pr.oclcNumber != null ) },
-        strategy: { pr, service -> return service.lookupViaConnector("rec.identifier=${pr.oclcNumber?.trim()}&startRecord=1&maximumRecords=1") }
+        strategy: { pr, service -> return service.lookupViaConnector("rec.identifier=${pr.oclcNumber?.trim()}&startRecord=1&maximumRecords=3") }
       ],
       [
         name:'Adapter_By_Title_And_Identifier',
         precondition: { pr -> return ( pr.isbn != null && pr.title != null ) },
-        strategy: { pr, service -> return service.lookupViaConnector("dc.title=${pr.title?.trim()} and bath.isbn=${pr.isbn?.trim()}&startRecord=1&maximumRecords=1") }
+        strategy: { pr, service -> return service.lookupViaConnector("dc.title=${pr.title?.trim()} and bath.isbn=${pr.isbn?.trim()}&startRecord=1&maximumRecords=3") }
       ],
       [
         name:'Adapter_By_Identifier',
         precondition: { pr -> return ( pr.isbn != null ) },
-        strategy: { pr, service -> return service.lookupViaConnector("bath.isbn=${pr.isbn?.trim()}&startRecord=1&maximumRecords=1") }
+        strategy: { pr, service -> return service.lookupViaConnector("bath.isbn=${pr.isbn?.trim()}&startRecord=1&maximumRecords=3") }
       ],
       [
         name:'Adapter_By_Title',
         precondition: { pr -> return ( pr.title != null ) },
-        strategy: { pr, service -> return service.lookupViaConnector("dc.title=${pr.title?.trim()}&startRecord=1&maximumRecords=1") }
+        strategy: { pr, service -> return service.lookupViaConnector("dc.title=${pr.title?.trim()}&startRecord=1&maximumRecords=3") }
       ],
     ]
   }
 
-  ItemLocation lookupViaConnector(String query) {
-    ItemLocation result = null;
+  List<ItemLocation> lookupViaConnector(String query) {
+
+    List<ItemLocation> result = [];
 
     //Override this method on BaseHost to use RTAC connector provided by IndexData
     AppSetting wms_connector_address = AppSetting.findByKey('wms_connector_address')
@@ -86,24 +87,24 @@ public class WmsHostLMSService extends BaseHostLMSService {
     }
     log.debug("Got Z3950 response: ${z_response}")
 
-    if ( z_response?.numberOfRecords == 1 ) {
-
-      // Got exactly 1 record
+    if ( z_response?.numberOfRecords?.size() > 0) {
       Map availability_summary = [:]
-      z_response?.records?.record?.recordData?.opacRecord?.holdings?.holding?.each { hld ->
+      result = z_response?.records?.record?.recordData?.opacRecord?.holdings?.holding?.findResults { hld ->
         if ( hld.circulations?.circulation?.availableNow?.@value=='1' ) {
-          log.debug("Available now");
+          log.debug("Holding available now");
           ItemLocation il = new ItemLocation( location: hld.localLocation, shelvingLocation:hld.shelvingLocation, callNumber:hld.callNumber )
-
-          if ( result == null ) 
-            result = il;
-
           availability_summary[hld.localLocation] = il;
+          return il;
+        } else {
+          log.debug("Holding unavailable");
+          return null;
         }
       }
-
       log.debug("At end, availability summary: ${availability_summary}");
+    } else {
+      log.debug("WMS connector lookup (${query}) returned no matches. Unable to determine availability.")
     }
+
     return result
   }
 }
