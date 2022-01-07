5bcf21470aa8400c892bcf3260a3fa0bd6a4fee5
Jason Skomorowski <jason@indexdata.com>
2022-01-07 16:10:46 -0500

    Record user id in audit entries if available PR-1016

--- service/grails-app/migrations/update-mod-rs-2-6.groovy
+++ service/grails-app/migrations/update-mod-rs-2-6.groovy
@@ -10,4 +10,10 @@ databaseChangeLog = {
         }
     }
 
+  changeSet(author: "jskomorowski (manual)", id: "2022-01-07-1655-001") {
+    addColumn(tableName: "patron_request_audit") {
+      column(name: "pra_user", type: "VARCHAR(36)")
+    }
+  }
+
 }
