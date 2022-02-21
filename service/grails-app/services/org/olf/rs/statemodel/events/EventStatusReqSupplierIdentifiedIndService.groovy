7b13ada72459b7007ce38b5658ae38816cb0faf5
Ian Ibbotson <ianibbo@gmail.com>
2022-02-21 10:01:16 +0000

    move trigger of new notice from EventNewpatronrequestIndService to EventStatusReqSupplierIdentifiedIndService

--- service/grails-app/services/org/olf/rs/statemodel/events/EventStatusReqSupplierIdentifiedIndService.groovy
+++ service/grails-app/services/org/olf/rs/statemodel/events/EventStatusReqSupplierIdentifiedIndService.groovy
@@ -1,10 +1,19 @@
 package org.olf.rs.statemodel.events;
 
 import org.olf.rs.statemodel.Events;
+import com.k_int.web.toolkit.refdata.RefdataValue;
 
 public class EventStatusReqSupplierIdentifiedIndService extends EventSendToNextLenderService {
+
+  PatronNoticeService patronNoticeService;
 
 	String name() {
 		return(Events.EVENT_STATUS_REQ_SUPPLIER_IDENTIFIED_INDICATION);
 	}
+
+  @Override
+  EventResultDetails processEvent(PatronRequest request, Map eventData, EventResultDetails eventResultDetails) {
+    patronNoticeService.triggerNotices(request, RefdataValue.lookupOrCreate('noticeTriggers', 'New request'));
+    return super.processEvent(request,eventData,eventResultDetails);
+  }
 }
