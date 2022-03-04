81e435e3000277c4e03ba3c2d349d634b51b7859
Ian Ibbotson <ianibbo@gmail.com>
2022-03-04 18:03:40 +0000

    Added HostLMSShelvingLocation.groovy

---
+++ service/grails-app/domain/org/olf/rs/HostLMSShelvingLocation.groovy
@@ -0,0 +1,47 @@
+package org.olf.rs
+
+import com.k_int.web.toolkit.refdata.RefdataValue
+import grails.gorm.multitenancy.Tenants;
+import grails.gorm.MultiTenant
+import java.time.LocalDateTime
+import org.olf.okapi.modules.directory.DirectoryEntry
+
+class HostLMSShelvingLocation implements MultiTenant<HostLMSShelvingLocation> {
+
+  HostLMSLocation location
+  String id
+  String code
+  String name
+  Date dateCreated
+  Date lastUpdated
+  // < 0 - Never use
+  // 0 - No preference / default
+  // > 0 - Preference order
+  Long supplyPreference
+
+  static constraints = {
+    location (nullable: false)
+    code (nullable: false)
+    name (nullable: true)
+    dateCreated (nullable: true, bindable: false)
+    lastUpdated (nullable: true, bindable: false)
+    supplyPreference (nullable: true)
+  }
+
+  static mapping = {
+    table 'host_lms_shelving_location'
+                               id column : 'hlsl_id', generator: 'uuid2', length:36
+                         location column : 'hlsl_host_lms_loc_fk'
+                          version column : 'hlsl_version'
+                             code column : 'hlsl_code'
+                             name column : 'hlsl_name'
+                      dateCreated column : 'hlsl_date_created'
+                      lastUpdated column : 'hlsl_last_updated'
+                 supplyPreference column : 'hlsl_supply_preference'
+  }
+
+  public String toString() {
+    return "HostLMSShelvingLocation: ${code}".toString()
+  }
+}
+
