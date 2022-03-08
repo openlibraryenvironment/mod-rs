c8ef3a78582cdc853e488bc3fa16661ca2d8722d
Chas <chas.woodfield@k-int.com>
2022-03-08 17:49:10 +0000

    A few more files linted

--- service/grails-app/services/org/olf/rs/statemodel/AbstractAction.groovy
+++ service/grails-app/services/org/olf/rs/statemodel/AbstractAction.groovy
@@ -5,59 +5,62 @@ import org.olf.rs.ReshareActionService;
 import org.olf.rs.ReshareApplicationEventHandlerService;
 import org.olf.rs.StatisticsService;
 
+/**
+ * The base class for all the actions
+ * @author Chas
+ *
+ */
 public abstract class AbstractAction {
 
-	private static final String POSSIBLE_FROM_STATES_QUERY='select distinct aa.actionCode from AvailableAction as aa where aa.fromState = :fromstate and aa.triggerType = :triggerType'
+    // We automatically inject these 3 services as some if not all actions use them
+    ReshareActionService reshareActionService;
+    ReshareApplicationEventHandlerService reshareApplicationEventHandlerService;
+    StatisticsService statisticsService;
 
-	// We automatically inject these 3 services as some if not all actions use them
-	ReshareActionService reshareActionService;
-	ReshareApplicationEventHandlerService reshareApplicationEventHandlerService;
-	StatisticsService statisticsService;
+    /**
+     * Method that all classes derive from this one that actually performs the action
+     * @param request The request the action is being performed against
+     * @param parameters Any parameters required for the action
+     * @param actionResultDetails The result of performing the action
+     * @return The actionResultDetails
+     */
+    abstract ActionResultDetails performAction(PatronRequest request, Object parameters, ActionResultDetails actionResultDetails);
 
-	/**
-	 * Method that all classes derive from this one that actually performs the action
-	 * @param request The request the action is being performed against
-	 * @param parameters Any parameters required for the action
-	 * @param actionResultDetails The result of performing the action
-	 * @return The actionResultDetails
-	 */
-	abstract ActionResultDetails performAction(PatronRequest request, Object parameters, ActionResultDetails actionResultDetails);
+    /**
+     * The name of the action
+     * @return the action name
+     */
+    abstract String name();
 
-	/**
-	 * The name of the action
-	 * @return the action name
-	 */
-	abstract String name();
+    /**
+     * Can this action lead to the state it came from ? We default to True, so override to return False if the action does not
+     * @return True if it can lead to the state it came from, otherwise False
+     */
+    Boolean canLeadToSameState() {
+        return(true);
+    }
 
-	/**
-	 * Can this action lead to the state it came from ? We default to True, so override to return False if the action does not
-	 * @return True if it can lead to the state it came from, otherwise False
-	 */
-	Boolean canLeadToSameState() {
-		return(true);
-	}
+    /**
+     * The states this action could lead to that are different from the state that it came from
+     * @return The states it could lead to
+     */
+    abstract String[] toStates();
 
-	/**
-	 * The states this action could lead to that are different from the state that it came from
-	 * @return The states it could lead to
-	 */
-	abstract String[] toStates();
+    /**
+     * All the possible states this action could lead to
+     * @return the possible states
+     */
+    String[] possibleToStates(String model) {
+        String[] states = canLeadToSameState() ? fromStates(model) : [];
+        return(states + toStates());
+    }
 
-	/**
-	 * All the possible states this action could lead to
-	 * @return the possible states
-	 */
-	String[] possibleToStates(String model) {
-		String[] states = canLeadToSameState() ? fromStates(model) : [];
-		return(states + toStates());
-	}
-
-	/**
-	 * The possible states that could lead to this action
-	 * @return The possible states that could lead to this action
-	 */
-	String[] fromStates(String model) {
-		String[] possibleStates = AvailableAction.getFromStates(model, name());
-		return(possibleStates);
-	}
+    /**
+     * The possible states that could lead to this action
+     * @return The possible states that could lead to this action
+     */
+    String[] fromStates(String model) {
+        String[] possibleStates = AvailableAction.getFromStates(model, name());
+        return(possibleStates);
+    }
 }
