5c3a0519fe49b0e5bfbe8427c02da0e21f4cb804
Ethan Freestone <ethan.freestone@k-int.co.uk>
2022-03-10 15:11:36 +0000

    feat: HostLMS delete safety
    Added ability for HostLMSLocation to check for PatronRequests linked to them before deletion

--- service/grails-app/controllers/mod/rs/HostLMSLocationController.groovy
+++ service/grails-app/controllers/mod/rs/HostLMSLocationController.groovy
@@ -11,12 +11,48 @@ import grails.gorm.multitenancy.CurrentTenant
 import groovy.util.logging.Slf4j
 import org.olf.rs.workflow.*;
 
+import grails.gorm.transactions.Transactional
+import static org.springframework.http.HttpStatus.*
+
+@CurrentTenant
 class HostLMSLocationController extends OkapiTenantAwareController<HostLMSLocation> {
   
   static responseFormats = ['json', 'xml']
   
   HostLMSLocationController() {
     super(HostLMSLocation)
+  }
+
+  @Transactional
+  def delete() {
+    HostLMSLocation loc = queryForResource(params.id)
+    
+    // Not found.
+    if (loc == null) {
+      transactionStatus.setRollbackOnly()
+      notFound()
+      return
+    }
+    
+
+    // Return the relevant status if not allowed to delete.
+    Map canDelete = loc.canDelete();
+
+    if (!canDelete.deleteValid) {
+      transactionStatus.setRollbackOnly()
+
+      Map response = [
+        message: "Host LMS Location is attached to Patron Request(s) ${canDelete.prs.join(', ')}".toString(),
+        linkedPatronRequests: canDelete.prs
+      ]
+
+      respond(response, status: METHOD_NOT_ALLOWED)
+      return
+    }
+    
+    // Finally delete the location if we get this far and respond.
+    deleteResource loc
+    render status: NO_CONTENT
   }
   
 }
