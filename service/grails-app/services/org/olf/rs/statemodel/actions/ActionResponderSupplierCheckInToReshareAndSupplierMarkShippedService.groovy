9bbd19ad358c5ae0b5b204880d966375a11f1911
Ethan Freestone <ethan.freestone@k-int.co.uk>
2022-03-18 13:18:34 +0000

    feat: New action service
    New service for combined action supplier check in to reshare and supplier mark shipped
    
    PR-1017

--- service/grails-app/services/org/olf/rs/statemodel/actions/ActionResponderSupplierCheckInToReshareAndSupplierMarkShippedService.groovy
+++ service/grails-app/services/org/olf/rs/statemodel/actions/ActionResponderSupplierCheckInToReshareAndSupplierMarkShippedService.groovy
@@ -23,7 +23,8 @@ import com.k_int.web.toolkit.settings.AppSetting;
  */
 public class ActionResponderSupplierCheckInToReshareAndSupplierMarkShippedService extends ActionResponderService {
 
-    private static final String VOLUME_STATUS_AWAITING_LMS_CHECK_OUT = 'awaiting_lms_check_out';
+    ActionResponderSupplierCheckInToReshareService actionResponderSupplierCheckInToReshareService;
+    ActionResponderSupplierMarkShippedService actionResponderSupplierMarkShippedService;
 
     private static final String REASON_SPOOFED = 'spoofed';
 
@@ -46,6 +47,65 @@ public class ActionResponderSupplierCheckInToReshareAndSupplierMarkShippedServic
 
     @Override
     ActionResultDetails performAction(PatronRequest request, Object parameters, ActionResultDetails actionResultDetails) {
-        
+      // Create ourselves an ActionResultDetails that we will pass to each of the actions we want to call
+      ActionResultDetails resultDetails = new ActionResultDetails();
+
+      // Default the result as being a success
+      resultDetails.result = ActionResult.SUCCESS;
+
+      String returnAuditMessage;
+
+      if (actionResponderSupplierCheckInToReshareService.performAction(request, parameters, resultDetails).result == ActionResult.SUCCESS) {
+        // Copy in the new status in case the second action fails
+        actionResultDetails.newStatus = resultDetails.newStatus;
+
+        // Store auditMessage from check in call in case of success
+        if (resultDetails.auditMessage) {
+          returnAuditMessage = "Combined action. Check in success: ${resultDetails.auditMessage}"
+
+          // Unset auditMessage on resultDetails so we don't get the same audit message twice
+          resultDetails.auditMessage = null;
+        }
+
+        // Now we can mark it as being shipped
+        if (actionResponderSupplierMarkShippedService.performAction(request, parameters, resultDetails).result == ActionResult.SUCCESS) {
+          // Its a success, so copy in the new status and response result
+          actionResultDetails.newStatus = resultDetails.newStatus;
+          actionResultDetails.responseResult = resultDetails.responseResult;
+
+          // Store auditMessage from check in call in case of success
+          if (resultDetails.auditMessage && returnAuditMessage) {
+            returnAuditMessage += " Mark item shipped success: ${resultDetails.auditMessage}.";
+          } else if (resultDetails.auditMessage) {
+            returnAuditMessage = "Combined action. Mark item shipped success: ${resultDetails.auditMessage}."
+          }
+        }
+      }
+      
+      // At least one of our two calls failed
+      if (resultDetails.result != ActionResult.SUCCESS) {
+        // Failed so copy back the appropriate details so it can be diagnosed
+        actionResultDetails.responseResult = resultDetails.responseResult;
+        actionResultDetails.responseResult = resultDetails.responseResult;
+        actionResultDetails.result = resultDetails.result;
+
+        // Audit trail is slightly different. We might have success information from the first call
+        if (resultDetails.auditMessage) {
+          if (returnAuditMessage) {
+            // The first call must have succeeded, so the second call has failed
+            returnAuditMessage += " Mark item shipped failed: ${resultDetails.auditMessage}"
+          } else {
+            // We don't know what has failed, only that something has
+            returnAuditMessage = "Combined action. Failure: ${resultDetails.auditMessage}"
+          }
+        }
+      }
+
+      // Even if both succeeded we may want audit message
+      if (returnAuditMessage) {
+        actionResultDetails.auditMessage = returnAuditMessage;
+      }
+
+      return(actionResultDetails);
     }
 }
