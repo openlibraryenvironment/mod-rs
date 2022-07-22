42d9aea4045b784d724c51b9f8a9b5ef4f8addab
Jason Skomorowski <jason@indexdata.com>
2022-07-22 11:27:09 -0400

    Backport adapter for mod-metastorage SI

--- service/src/main/groovy/org/olf/rs/referenceData/RefdataValueData.groovy
+++ service/src/main/groovy/org/olf/rs/referenceData/RefdataValueData.groovy
@@ -100,6 +100,7 @@ public class RefdataValueData {
 
     // Shared index adapter
     public static final String SHARED_INDEX_ADAPTER_FOLIO = 'FOLIO';
+    public static final String SHARED_INDEX_ADAPTER_METASTORAGE = 'modmetastorage';
 
     // Yes / No
     public static final String YES_NO_NO  = 'No';
@@ -183,6 +184,7 @@ public class RefdataValueData {
 
 
             RefdataValue.lookupOrCreate(VOCABULARY_SHARED_INDEX_ADAPTER, SHARED_INDEX_ADAPTER_FOLIO);
+            RefdataValue.lookupOrCreate(VOCABULARY_SHARED_INDEX_ADAPTER, 'mod-meta-storage', SHARED_INDEX_ADAPTER_METASTORAGE);
 
             RefdataValue.lookupOrCreate(VOCABULARY_PATRON_STORE_ADAPTER, PATRON_STORE_ADAPTER_FOLIO);
             RefdataValue.lookupOrCreate(VOCABULARY_PATRON_STORE_ADAPTER, PATRON_STORE_ADAPTER_MANUAL);
