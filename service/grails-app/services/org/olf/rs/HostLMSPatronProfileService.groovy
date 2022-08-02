b9a786510b2bbb75d8feef16119525f528c38f70
Ian Ibbotson <ianibbo@gmail.com>
2022-08-02 06:29:44 +0100

    Tidy up

--- service/grails-app/services/org/olf/rs/HostLMSPatronProfileService.groovy
+++ service/grails-app/services/org/olf/rs/HostLMSPatronProfileService.groovy
@@ -8,42 +8,41 @@ import org.hibernate.Transaction;
  */
 public class HostLMSPatronProfileService {
 
-    PatronNoticeService patronNoticeService;
+  PatronNoticeService patronNoticeService;
 
-    /**
-     * Given a code and name looks to see if the HostLMSPatronProfile record already exists and if not creates it
-     * If it it does exist, it ensures that it is active
-     * @param code The code for the profile
-     * @param name The name for the profile
-     * @return The record that represents this code and name
-     */
-    public HostLMSPatronProfile ensureActive(String code, String name) {
+  /**
+    * Given a code and name looks to see if the HostLMSPatronProfile record already exists and if not creates it
+    * If it it does exist, it ensures that it is active
+    * @param code The code for the profile
+    * @param name The name for the profile
+    * @return The record that represents this code and name
+    */
+  public HostLMSPatronProfile ensureActive(String code, String name) {
 
-        // N.B. You can't really return an object which was materialised in one transactional context to a session
-        // living in a different transactional context.
-        HostLMSPatronProfile patronProfile;
+    // N.B. You can't really return an object which was materialised in one transactional context to a session
+    // living in a different transactional context.
+    HostLMSPatronProfile patronProfile;
 
-            try {
-              // Start a new transaction
-              HostLMSPatronProfile.withTransaction { status ->
+    try {
+      // Start a new transaction
+      HostLMSPatronProfile.withTransaction { status ->
 
-                patronProfile = HostLMSPatronProfile.findByCode(code);
+        patronProfile = HostLMSPatronProfile.findByCode(code);
 
-                if (patronProfile == null) {
-                  patronProfile = new HostLMSPatronProfile(code: code, name: name);
-                  patronProfile.save(flush:true, failOnError:true);
-                  // Trigger a notice to be sent if it has been configured
-                  patronNoticeService.triggerNotices(patronProfile);
-                } else if (patronProfile.hidden == true) {
-                  // Unhide it as it is active again
-                  patronProfile.hidden = false;
-                  patronProfile.save(flush:true, failOnError:true);
-                }
-              }
-            } catch(Exception e) {
-                log.error('Exception thrown while creating / updating HostLMSPatronProfile: ' + code, e);
-            }
+        if (patronProfile == null) {
+          patronProfile = new HostLMSPatronProfile(code: code, name: name);
+          patronProfile.save(flush:true, failOnError:true);
+          // Trigger a notice to be sent if it has been configured
+          patronNoticeService.triggerNotices(patronProfile);
+        } else if (patronProfile.hidden == true) {
+          // Unhide it as it is active again
+          patronProfile.hidden = false;
+          patronProfile.save(flush:true, failOnError:true);
         }
-        return(patronProfile);
+      }
+    } catch(Exception e) {
+      log.error('Exception thrown while creating / updating HostLMSPatronProfile: ' + code, e);
     }
+    return(patronProfile);
+  }
 }
