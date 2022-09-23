dba8c4afba4445238f5a6df687f85cde06ac74ae
Jason Skomorowski <jason@indexdata.com>
2022-09-23 15:56:26 -0400

    Minor tidying for horizon test
    Also use zresponse from an id query rather than isbn so it'll work for
    both purposes.

--- service/src/test/groovy/org/olf/rs/HorizonHostLMSServiceSpec.groovy
+++ service/src/test/groovy/org/olf/rs/HorizonHostLMSServiceSpec.groovy
@@ -1,19 +1,14 @@
 package org.olf.rs
 
 import grails.testing.services.ServiceUnitTest
-import io.swagger.util.Json
 import org.olf.rs.hostlms.HorizonHostLMSService
-import org.olf.rs.lms.ItemLocation
 import spock.lang.Specification
-import groovy.util.logging.Slf4j
 import groovy.json.JsonOutput
 import groovy.util.XmlSlurper
 
-@Slf4j
 class HorizonHostLMSServiceSpec extends Specification implements ServiceUnitTest<HorizonHostLMSService> {
     def 'extractAvailableItemsFrom'() {
         setup:
-        log.info(new File('.').absolutePath);
         def parsedSample = new XmlSlurper().parseText(new File('src/test/resources/zresponsexml/horizon-jhu.xml').text);
 
         when: 'We extract holdings'
