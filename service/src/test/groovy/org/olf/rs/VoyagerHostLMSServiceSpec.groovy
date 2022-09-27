c5387f0c981872138ed041cbbc20f84868a6e2f3
Jason Skomorowski <jason@indexdata.com>
2022-09-27 18:12:47 -0400

    Extract itemId in VoyagerHostLMSService PR-1359

---
+++ service/src/test/groovy/org/olf/rs/VoyagerHostLMSServiceSpec.groovy
@@ -0,0 +1,22 @@
+package org.olf.rs
+
+import grails.testing.services.ServiceUnitTest
+import groovy.json.JsonOutput
+import org.olf.rs.hostlms.VoyagerHostLMSService
+import spock.lang.Specification
+
+class VoyagerHostLMSServiceSpec extends Specification implements ServiceUnitTest<VoyagerHostLMSService> {
+    def 'extractAvailableItemsFrom'() {
+        setup:
+        def parsedSample = new XmlSlurper().parseText(new File('src/test/resources/zresponsexml/voyager-yale.xml').text);
+
+        when: 'We extract holdings'
+        def result = service.extractAvailableItemsFrom(parsedSample);
+
+        then:
+        def resultJson = JsonOutput.toJson(result.values().first());
+        result.size() == 1;
+        resultJson == '{"itemId":"211398","shelvingLocation":null,"callNumber":"PS3561 O646 W5 1979 (LC)","reason":null,"shelvingPreference":null,"preference":null,"location":"Library Shelving Facility (LSF)","itemLoanPolicy":null}';
+    }
+}
+
