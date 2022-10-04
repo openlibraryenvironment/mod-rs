4aff72f881d9117a545f3d65697abd40d58c75f5
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2022-10-04 11:22:16 +0100

    chore(Migrations): Include the features we want to pull in from TK

--- service/grails-app/migrations/module-tenant-changelog.groovy
+++ service/grails-app/migrations/module-tenant-changelog.groovy
@@ -8,4 +8,9 @@ databaseChangeLog = {
   include file: 'update-mod-rs-2-7.groovy'
   include file: 'update-mod-rs-2-8.groovy'
   include file: 'update-mod-rs-2-9.groovy'
+  
+  // Toolkit features opted into.
+  include file: 'wtk/additional_CustomPropertyDefinitions.feat.groovy'
+  include file: 'wtk/multi-value-custprops.feat.groovy'
+  include file: 'wtk/hidden-appsetting.feat.groovy'
 }
