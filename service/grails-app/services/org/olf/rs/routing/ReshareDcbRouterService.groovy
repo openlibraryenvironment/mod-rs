933bc487d14081a949ee5c7c4547c2b3b8fe5dad
Ian Ibbotson <ianibbo@googlemail.com>
2022-11-07 18:45:01 +0000

    progress on dcb router

--- service/grails-app/services/org/olf/rs/routing/ReshareDcbRouterService.groovy
+++ service/grails-app/services/org/olf/rs/routing/ReshareDcbRouterService.groovy
@@ -7,6 +7,7 @@ import org.olf.okapi.modules.directory.Symbol;
 import groovy.json.JsonOutput;
 import org.olf.rs.DirectoryEntryService
 import org.olf.rs.SharedIndexActions;
+import org.olf.rs.AvailabilityStatement;
 
 
 public class ReshareDcbRouterService implements RequestRouter {
@@ -22,50 +23,50 @@ public class ReshareDcbRouterService implements RequestRouter {
     log.debug("ReshareDcbRouterService::findMoreSuppliers(${description},${already_tried_symbols})");
     List<RankedSupplier> result = []
 
-    AppSetting static_routing_cfg = AppSetting.findByKey('static_routes')
-    String static_routing_str = static_routing_cfg?.value
-    if ( static_routing_str ) {
-      String[] components = static_routing_str.split(',')  // TYPE:NAMESPACE:SYMBOL,TYPE:NAMESPACE:SYMBOL
-      components.each { static_option ->
-        String[] option_parts = static_option.split(':');
-        Symbol s
-        String symbolString = ''
 
-        if ( option_parts.size() == 2 ) {
-          // We assume SYMBOL if only 2 parts
-          s = directoryEntryService.resolveSymbol(option_parts[0], option_parts[1]);
-          symbolString = "${option_parts[0]}:${option_parts[1]}" 
-        }
-        else if ( option_parts.size() == 3 ) {
-          if ( option_parts[0] == 'SYMBOL' ) {
-            s = directoryEntryService.resolveSymbol(option_parts[1], option_parts[2]);
-            symbolString = "${option_parts[1]}:${option_parts[2]}" 
-          }
-          else {
-            log.warn("Unhandled static routing option: ${option_parts[0]} in ${static_option} of ${static_routing_str}");
-          }
+    // Probably we should iterate around the list of resource sharing contexts for the requesting location.... until then
+    List<AvailabilityStatement> availability = reshareDcbSharedIndexService.findAppropriateCopies(description);
+
+    log.debug("Got DCB availability: ${availability}");
+
+    // our pretend rota consists of 2 locations 
+    ['ISIL:DST2', 'ISIL:DST3'].each { option ->
+      String[] option_parts = option.split(':');
+      Symbol s = null;
+      String symbolString = null;
+
+      if ( option_parts.size() == 2 ) {
+        // We assume SYMBOL if only 2 parts
+        s = directoryEntryService.resolveSymbol(option_parts[0], option_parts[1]);
+        symbolString = "${option_parts[0]}:${option_parts[1]}" 
+      }
+      else if ( option_parts.size() == 3 ) {
+        if ( option_parts[0] == 'SYMBOL' ) {
+          s = directoryEntryService.resolveSymbol(option_parts[1], option_parts[2]);
+          symbolString = "${option_parts[1]}:${option_parts[2]}" 
         }
         else {
-          log.warn("Unable to parse TYPE:NAMESPACE:VALUE triple in ${static_option} of ${static_routing_str}");
-        }
-
-        // At this point we should have a Symbol s we can use to add entries to the rota
-        if (s && directoryEntryService.directoryEntryIsLending(s.owner)) {
-          result.add(new RankedSupplier(supplier_symbol:symbolString,
-                                        copy_identifier:'', 
-                                        instance_identifier:'',
-                                        ill_policy:'Will lend',
-                                        rank:0, 
-                                        rankReason:'static rank'));
-
+          log.warn("Unhandled static routing option: ${option_parts[0]} in ${static_option} of ${static_routing_str}");
         }
       }
-    }
-    else {
-      log.warn("Unable to locate static routing config option (static_routes) - should be formatted as a comma separated string of options");
+      else {
+          log.warn("Unable to parse TYPE:NAMESPACE:VALUE triple in ${static_option} of ${static_routing_str}");
+      }
+
+      // At this point we should have a Symbol s we can use to add entries to the rota
+      if (s && directoryEntryService.directoryEntryIsLending(s.owner)) {
+        result.add(new RankedSupplier(
+                                      supplier_symbol:symbolString,
+                                      copy_identifier:'', 
+                                      instance_identifier:'',
+                                      ill_policy:'Will lend',
+                                      rank:0, 
+                                      rankReason:'static rank'));
+
+      }
     }
 
-    log.debug("StaticRouterService::findMoreSuppliers returns ${result}");
+    log.debug("ReshareDcbRouterService::findMoreSuppliers returns ${result}");
     return result
   }
 
