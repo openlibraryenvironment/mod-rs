fca36557f4be25337e45d5acaae7606eedb8d7bf
Chas <chas.woodfield@k-int.com>
2023-01-25 14:56:19 +0000

    Added application settings to swagger

--- service/grails-app/controllers/mod/rs/SettingController.groovy
+++ service/grails-app/controllers/mod/rs/SettingController.groovy
@@ -1,11 +1,17 @@
 package mod.rs
 
-import com.k_int.okapi.OkapiTenantAwareController
-import com.k_int.web.toolkit.settings.AppSetting
+import com.k_int.web.toolkit.settings.AppSetting;
 
-import grails.gorm.transactions.Transactional
+import grails.gorm.transactions.Transactional;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
-class SettingController extends OkapiTenantAwareController<AppSetting> {
+@Api(value = "/rs/settings/appSettings", tags = ["Application Settings Controller"], description = "API for all things to do with application settings")
+class SettingController extends OkapiTenantAwareSwaggerController<AppSetting> {
 
     static responseFormats = ['json', 'xml'];
 
@@ -15,6 +21,89 @@ class SettingController extends OkapiTenantAwareController<AppSetting> {
 
     @Override
     @Transactional
+    @ApiOperation(
+        value = "Search with the supplied criteria",
+        nickname = "/",
+        produces = "application/json",
+        httpMethod = "GET"
+    )
+    @ApiResponses([
+        @ApiResponse(code = 200, message = "Success")
+    ])
+    @ApiImplicitParams([
+        @ApiImplicitParam(
+            name = "term",
+            paramType = "query",
+            required = false,
+            allowMultiple = false,
+            value = "The term to be searched for",
+            dataType = "string"
+        ),
+        @ApiImplicitParam(
+            name = "match",
+            paramType = "query",
+            required = false,
+            allowMultiple = true,
+            value = "The properties the match is to be applied to",
+            dataType = "string"
+        ),
+        @ApiImplicitParam(
+            name = "filters",
+            paramType = "query",
+            required = false,
+            allowMultiple = true,
+            value = "The filters to be applied",
+            dataType = "string"
+        ),
+        @ApiImplicitParam(
+            name = "sort",
+            paramType = "query",
+            required = false,
+            allowMultiple = true,
+            value = "The properties to sort the items by",
+            dataType = "string"
+        ),
+        @ApiImplicitParam(
+            name = "max",
+            paramType = "query",
+            required = false,
+            allowMultiple = false,
+            value = "Maximum number of items to return",
+            dataType = "int"
+        ),
+        @ApiImplicitParam(
+            name = "perPage",
+            paramType = "query",
+            required = false,
+            allowMultiple = false,
+            value = "Number of items per page",
+            dataType = "int"
+        ),
+        @ApiImplicitParam(
+            name = "offset",
+            paramType = "query",
+            required = false,
+            allowMultiple = false,
+            value = "Offset from the becoming of the result set to start returning results",
+            dataType = "int"
+        ),
+        @ApiImplicitParam(
+            name = "page",
+            paramType = "query",
+            required = false,
+            allowMultiple = false,
+            value = "The page you wnat the results being returned from",
+            dataType = "int"
+        ),
+        @ApiImplicitParam(
+            name = "stats",
+            paramType = "query",
+            required = false,
+            allowMultiple = false,
+            value = "Do we return statistics about the search",
+            dataType = "boolean"
+        )
+    ])
     def index(Integer max) {
         // Use gorm criteria builder to always add your custom filter....
         Closure gormFilterClosure = {
