e369baaf3e65f422c44b40f1d173b5f4efe38946
Chas <chas.woodfield@k-int.com>
2023-01-31 14:44:18 +0000

    Tests for Application, App Settings and available action

---
+++ service/src/integration-test/groovy/org/olf/ApplicationSpec.groovy
@@ -0,0 +1,64 @@
+package org.olf
+
+import grails.testing.mixin.integration.Integration;
+import groovy.util.logging.Slf4j;
+import spock.lang.Stepwise;
+
+@Slf4j
+@Integration
+@Stepwise
+class ApplicationSpec extends TestBase {
+
+    // This method is declared in the HttpSpec
+    def setupSpecWithSpring() {
+        super.setupSpecWithSpring();
+    }
+
+    def setupSpec() {
+    }
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "Set up test tenants"(String tenantId, String name) {
+        when:"We post a new tenant request to the OKAPI controller"
+            boolean response = setupTenant(tenantId, name);
+
+        then:"The response is correct"
+            assert(response);
+
+        where:
+            tenantId   | name
+            TENANT_ONE | TENANT_ONE
+    }
+
+    void "Fetch the application home page"(String tenantId, String ignore) {
+        when:"Fetch the Applicarion home page"
+
+            // Set the headers
+            setHeaders([ 'X-Okapi-Tenant': tenantId ]);
+
+            // Fetch the AppSetting
+            def response = doGet("${baseUrl}/");
+            log.debug("Response from Get application: " + response.toString());
+
+        then:"Check we have a valid response"
+            // Check the various fields
+            assert(response != null);
+            assert(response.message != null);
+            assert(response.grailsversion != null);
+            assert(response.groovyversion != null);
+            assert(response.jvmversion != null);
+            assert(response.appversion != null);
+            assert(response.artefacts != null);
+            assert(response.controllers != null);
+            assert(response.plugins != null);
+
+        where:
+            tenantId   | ignore
+            TENANT_ONE | ""
+    }
+}
