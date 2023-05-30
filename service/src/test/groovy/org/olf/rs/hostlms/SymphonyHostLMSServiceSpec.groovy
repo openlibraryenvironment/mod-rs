13b04c5b112977d610072a34159e9448ae55cbd9
Kurt Nordstrom <kurt.e.nordstrom@gmail.com>
2023-05-30 15:43:28 -0400

    Ports for Symphony adapter changes, NcipFromAgencyAuthentication password field, unique request id for local requests

--- service/src/test/groovy/org/olf/rs/hostlms/SymphonyHostLMSServiceSpec.groovy
+++ service/src/test/groovy/org/olf/rs/hostlms/SymphonyHostLMSServiceSpec.groovy
@@ -18,5 +18,24 @@ class SymphonyHostLMSServiceSpec extends Specification implements ServiceUnitTes
         result.size() == 1;
         resultJson == '{"temporaryShelvingLocation":null,"itemId":null,"temporaryLocation":null,"shelvingLocation":"STACKS","callNumber":"HV6534 .V55 J36 2017","reason":null,"shelvingPreference":null,"preference":null,"location":"SAL3","itemLoanPolicy":"STKS-MONO"}';
     }
+
+    def 'modifyIdentifier'() {
+        setup:
+        def identifier = 'a983475243';
+        def identifier2 = '983475243';
+        def identifier3 = 'abcdefgHIJKL';
+
+        when: 'We modify the identifier'
+        def result = service.modifyIdentifier(identifier);
+        def result2 = service.modifyIdentifier(identifier2);
+        def result3 = service.modifyIdentifier(identifier3);
+
+        then:
+        '^C983475243' == result;
+        '^C983475243' == result2;
+        '^CabcdefgHIJKL' == result3;
+
+
+    }
 }
 
