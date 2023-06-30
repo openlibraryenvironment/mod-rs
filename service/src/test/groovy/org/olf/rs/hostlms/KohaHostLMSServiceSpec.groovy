04ab9a35bfdd8255fbb248ef88595af7aa73609f
Kurt Nordstrom <kurt.e.nordstrom@gmail.com>
2023-06-30 15:51:00 -0400

    Koha autoresponder will not select holdings with the 'q' field populated

--- service/src/test/groovy/org/olf/rs/hostlms/KohaHostLMSServiceSpec.groovy
+++ service/src/test/groovy/org/olf/rs/hostlms/KohaHostLMSServiceSpec.groovy
@@ -19,5 +19,18 @@ class KohaHostLMSServiceSpec extends Specification implements ServiceUnitTest<Ko
         result.size() == 1;
         resultJson == '{"temporaryShelvingLocation":null,"itemId":null,"temporaryLocation":null,"shelvingLocation":"CIRC2","callNumber":"580.1 G733v","reason":null,"shelvingPreference":null,"preference":null,"location":"CHATHAM","itemLoanPolicy":null}';
     }
+
+    def 'failToExtractAvailableItemsFrom'() {
+        setup:
+        def parsedSample = new XmlSlurper().parseText(new File('src/test/resources/zresponsexml/koha-chatham_checked_out.xml').text);
+
+        when: 'We extract holdings'
+        def result = service.extractAvailableItemsFrom(parsedSample, null, new DoNothingHoldingLogDetails());
+
+        then:
+        def resultJson = JsonOutput.toJson(result);
+        result.size() == 0;
+    }
+
 }
 
