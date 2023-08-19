b95fd2574b26f0c770fec80d1f50656c0efb963c
Jason Skomorowski <jason@indexdata.com>
2023-08-19 12:43:50 -0400

    Sort pull slip batches printed by timer PR-1528

--- service/grails-app/services/org/olf/rs/timers/TimerPrintPullSlipsService.groovy
+++ service/grails-app/services/org/olf/rs/timers/TimerPrintPullSlipsService.groovy
@@ -31,6 +31,7 @@ where pr.pickLocation.id in ( :loccodes ) and
                        inner join sm.states as sms
                    where sm = pr.stateModel and
                          sms.triggerPullSlipEmail = true)
+order by pr.pickLocation.code, pr.title
 ''';
 
     private static final String PULL_SLIP_SUMMARY = '''
