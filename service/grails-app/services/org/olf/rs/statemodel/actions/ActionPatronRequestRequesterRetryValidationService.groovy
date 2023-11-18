97730aba735747bd713e3403d70fc4b1c1d407a3
Kurt Nordstrom <kurt.e.nordstrom@gmail.com>
2023-11-18 11:06:31 -0500

    Rework ActionPatronRequestRequesterRetryValidationService to extend A… (#597)
    * Rework ActionPatronRequestRequesterRetryValidationService to extend ActionPatronRequestEditService
    
    * Tweak logic to check contents of parameters
    
    * Simplify check for updated fields

--- service/grails-app/services/org/olf/rs/statemodel/actions/ActionPatronRequestRequesterRetryValidationService.groovy
+++ service/grails-app/services/org/olf/rs/statemodel/actions/ActionPatronRequestRequesterRetryValidationService.groovy
@@ -4,8 +4,10 @@ package org.olf.rs.statemodel.actions
 import grails.gorm.multitenancy.Tenants
 import org.olf.rs.EventPublicationService
 import org.olf.rs.PatronRequest
+import org.olf.rs.iso18626.NoteSpecials
 import org.olf.rs.shared.TenantSymbolMapping;
 import org.olf.rs.statemodel.AbstractAction;
+import org.olf.rs.patronRequest.PickupLocationService;
 import org.olf.rs.statemodel.ActionResultDetails;
 import org.olf.rs.statemodel.Actions
 import org.olf.rs.statemodel.Events;
@@ -15,10 +17,11 @@ import org.olf.rs.statemodel.Events;
  * Action to send a request back to the "new request" state to be re-tried
  */
 
-public class ActionPatronRequestRequesterRetryValidationService extends AbstractAction {
+public class ActionPatronRequestRequesterRetryValidationService extends ActionPatronRequestEditService {
 
 
     EventPublicationService eventPublicationService;
+    PickupLocationService pickupLocationService;
 
     @Override
     String name() {
@@ -26,7 +29,30 @@ public class ActionPatronRequestRequesterRetryValidationService extends Abstract
     }
 
     @Override
-    ActionResultDetails performAction(PatronRequest request, Object parameters, ActionResultDetails actionResultDetails) {
+    ActionResultDetails performAction(PatronRequest request,
+                                      Object parameters, ActionResultDetails actionResultDetails) {
+        log.debug("Updating/Retrying request ${request.id} with parameters ${parameters}");
+        StringBuffer auditMessage = new StringBuffer("Record has been edited:");
+        boolean updateFields = !parameters.isEmpty();
+        log.debug("updateFields is ${updateFields}");
+
+        if (updateFields) {
+            updateableFields.each() { fieldDetails ->
+                log.debug("Updating field prior to retrying ${fieldDetails.field} in request ${request.id}");
+                // Update the field
+                if (updateField(request, parameters, fieldDetails.field, auditMessage, fieldDetails.isDate)) {
+                    // It has changed
+                    if (fieldDetails.doPickupCheck) {
+                        // Perform the appropriate checks on the pickup location
+                        pickupLocationService.check(request);
+                    }
+                }
+            }
+            request.save();
+        }
+
+
+
         actionResultDetails.responseResult.status = true;
 
         String tenant = Tenants.currentId();
@@ -46,7 +72,26 @@ public class ActionPatronRequestRequesterRetryValidationService extends Abstract
                 ]
         );
 
+        if (updateFields) {
+            actionResultDetails.auditMessage = auditMessage.toString();
+            parameters.audit = null;
+            parameters.batches = null;
+            parameters.conditions = null;
+            parameters.notifications = null;
+            parameters.requestIdentifiers = null;
+            parameters.rota = null;
+            parameters.tags = null;
+            parameters.volumes = null;
+
+            // Specific fields
+            parameters.lastProtocolData = null;
+            parameters.resolvedPatron = null;
+            parameters.resolvedPickupLocation = null;
+            parameters.resolvedRequester = null;
+            parameters.resolvedSupplier = null;
+        }
 
         return(actionResultDetails);
     }
+    
 }
