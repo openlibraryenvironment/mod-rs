2ac082e3feba9077b448047ab10df471dea46436
Edijs Timuls <edijs@indexdata.com>
2024-01-30 16:06:28 +0000

    PR-1629 - Horizon's Host LMS adapter needs to change for the z39.50 lookup to use @attr 1=12 instead of @attr 1=100.

--- service/grails-app/services/org/olf/rs/hostlms/HorizonHostLMSService.groovy
+++ service/grails-app/services/org/olf/rs/hostlms/HorizonHostLMSService.groovy
@@ -1,11 +1,11 @@
-package org.olf.rs.hostlms;
+package org.olf.rs.hostlms
 
-import org.olf.rs.PatronRequest;
-import org.olf.rs.circ.client.CirculationClient;
-import org.olf.rs.circ.client.NCIPClientWrapper;
-import org.olf.rs.lms.ItemLocation;
-import org.olf.rs.logging.IHoldingLogDetails;
-import org.olf.rs.settings.ISettings;
+
+import org.olf.rs.circ.client.CirculationClient
+import org.olf.rs.circ.client.NCIPClientWrapper
+import org.olf.rs.lms.ItemLocation
+import org.olf.rs.logging.IHoldingLogDetails
+import org.olf.rs.settings.ISettings
 
 public class HorizonHostLMSService extends BaseHostLMSService {
 
@@ -38,39 +38,5 @@ public class HorizonHostLMSService extends BaseHostLMSService {
       log.warn("HorizonHostLMSService expected the response to contain an OPAC record, but none was found");
     }
     return availability_summary;
-  }
-
-  public static List<String> splitLocation(String loc) {
-    def pattern = /(.+)\s+-\s+(.+)/;
-    def matcher = loc =~ pattern;
-    if(matcher.find()) {
-      return [ matcher.group(1), matcher.group(2)];
-    }
-    return null;
-  }
-
-  @Override
-  public List<ItemLocation> z3950ItemsByIdentifier(PatronRequest pr, ISettings settings, IHoldingLogDetails holdingLogDetails) {
-
-    List<ItemLocation> result = [];
-
-    def prefix_query_string = "@attr 1=100 ${pr.supplierUniqueRecordId}";
-    def z_response = z3950Service.query(settings, prefix_query_string, 1, getHoldingsQueryRecsyn(), holdingLogDetails);
-    log.debug("Got Z3950 response: ${z_response}");
-
-    if ( z_response?.numberOfRecords == 1 ) {
-      // Got exactly 1 record
-      List<ItemLocation> availability_summary = extractAvailableItemsFrom(z_response,"Match by @attr 1=100 ${pr.supplierUniqueRecordId}", holdingLogDetails)
-      if ( availability_summary?.size() > 0 ) {
-        result = availability_summary;
-      }
-      else {
-        log.debug("CQL lookup(${prefix_query_string}) returned ${z_response?.numberOfRecords} matches. Unable to determine availability");
-      }
-
-      log.debug("At end, availability summary: ${availability_summary}");
-    }
-
-    return result;
   }
 }
