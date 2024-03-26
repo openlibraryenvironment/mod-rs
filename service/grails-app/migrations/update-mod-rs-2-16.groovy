b3b4a7a0e411847f9587a5309f0947a3be4c77af
Jason Skomorowski <jason@indexdata.com>
2024-03-26 19:04:06 -0400

    Remove non-standard copy-non-returnable serviceType PR-1611

--- service/grails-app/migrations/update-mod-rs-2-16.groovy
+++ service/grails-app/migrations/update-mod-rs-2-16.groovy
@@ -4,10 +4,17 @@ databaseChangeLog = {
             column(name: "pr_pages_requested", type: "varchar(255)")
         }
     }
+
     changeSet(author: "jskomorowski", id: "20240315-1030-001") {
         addColumn(tableName: "patron_request") {
             column(name:'pr_copyright_type_fk', type: "varchar(36)")
         }
         addForeignKeyConstraint(baseColumnNames: "pr_copyright_type_fk", baseTableName: "patron_request", constraintName: "FK_pr_copyright_type", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "rdv_id", referencedTableName: "refdata_value")
+    }
+
+    changeSet(author: "jskomorowski", id: "20240326-1700-001") {
+        delete(tableName: 'refdata_value') {
+            where("rdv_value='copy-non-returnable'")
+        }
     }
 }
