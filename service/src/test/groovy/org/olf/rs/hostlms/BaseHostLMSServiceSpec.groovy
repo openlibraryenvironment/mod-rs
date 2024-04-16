fb97c9b21c1f46e16a24a052b409a6e6803067c6
skomorokh <skomorokh@users.noreply.github.com>
2024-04-16 10:22:03 -0400

    Patron profiles for Evergreen PR-1691 (#661)

--- service/src/test/groovy/org/olf/rs/hostlms/BaseHostLMSServiceSpec.groovy
+++ service/src/test/groovy/org/olf/rs/hostlms/BaseHostLMSServiceSpec.groovy
@@ -15,10 +15,14 @@ class BaseHostLMSServiceSpec extends Specification implements ServiceUnitTest<De
         then:
         def responseJsonObject = new JSONObject(responseJson);
         service.processLookupUserResponse(result, responseJsonObject, new DoNothingNcipLogDetails());
+
+        expect:
         result.result == true;
+        result.userProfile;
 
         where:
-        adapter | responseJson
-        'WMS'   | '{"firstName":"TEST ACCOUNT","lastName":"OCLC","privileges":[{"value":"OK","key":"STATUS"}],"electronicAddresses":[{"value":"rebecca.gerber1@umassmed.edu","key":"emailAddress"}],"physicalAddresses":[{"value":{"postalCode":"01545","locality":"Shrewsbury","lineOne":"222 Maple Ave","region":"MA"},"key":"unknown-type"}],"userId":"D760463647"}'
+        adapter       | responseJson
+        'WMS'         | '{"firstName":"TEST ACCOUNT","lastName":"OCLC","privileges":[{"key":"STATUS","value":"OK"}, {"key":"PROFILE","value":"A Profile"}],"electronicAddresses":[{"value":"rebecca.gerber1@umassmed.edu","key":"emailAddress"}],"physicalAddresses":[{"value":{"postalCode":"01545","locality":"Shrewsbury","lineOne":"222 Maple Ave","region":"MA"},"key":"unknown-type"}],"userId":"D760463647"}'
+        'Evergreen'   | '{"firstName":"Firstly","Lastly":"","privileges":[{"key":"Some Profile","value":"Active"}],"electronicAddresses":[{"value":"rebecca.gerber1@umassmed.edu","key":"emailAddress"}],"physicalAddresses":[{"value":{"postalCode":"01545","locality":"Shrewsbury","lineOne":"222 Maple Ave","region":"MA"},"key":"unknown-type"}],"userId":"D760463647"}'
     }
 }
