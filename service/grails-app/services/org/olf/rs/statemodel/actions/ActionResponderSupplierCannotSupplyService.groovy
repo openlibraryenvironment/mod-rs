b08ead62287a2c8f1838f8c8343fc615641b36c7
Kurt Nordstrom <kurt.e.nordstrom@gmail.com>
2024-04-26 14:29:37 -0400

    Pr 1708 (#668)
    * Add sending of CancelRequestItem (when relevant) on request cancel
    
    * Make cancel request item conditional on not responding 'no' to cancel request
    
    * Add back missing import
    
    * Add check for CancelRequestItem to SupplierCannotSupply action
    
    * Add missing imports/declarations

--- service/grails-app/services/org/olf/rs/statemodel/actions/ActionResponderSupplierCannotSupplyService.groovy
+++ service/grails-app/services/org/olf/rs/statemodel/actions/ActionResponderSupplierCannotSupplyService.groovy
@@ -1,6 +1,9 @@
-package org.olf.rs.statemodel.actions;
+package org.olf.rs.statemodel.actions
 
-import org.olf.rs.PatronRequest;
+import org.olf.rs.HostLMSService;
+import org.olf.rs.PatronRequest
+import org.olf.rs.SettingsService
+import org.olf.rs.referenceData.SettingsData;
 import org.olf.rs.statemodel.AbstractAction;
 import org.olf.rs.statemodel.ActionResultDetails;
 import org.olf.rs.statemodel.Actions;
@@ -12,6 +15,11 @@ import org.olf.rs.statemodel.Actions;
  */
 public class ActionResponderSupplierCannotSupplyService extends AbstractAction {
 
+    private static final String SETTING_REQUEST_ITEM_NCIP = "ncip";
+
+    SettingsService settingsService;
+    HostLMSService hostLMSService;
+
     @Override
     String name() {
         return(Actions.ACTION_RESPONDER_SUPPLIER_CANNOT_SUPPLY);
@@ -21,6 +29,15 @@ public class ActionResponderSupplierCannotSupplyService extends AbstractAction {
     ActionResultDetails performAction(PatronRequest request, Object parameters, ActionResultDetails actionResultDetails) {
         // Just send the message of unfilled
         reshareActionService.sendResponse(request, 'Unfilled', parameters, actionResultDetails);
+
+        log.debug("Checking to see if we need to send a CancelRequestItem");
+        if (settingsService.hasSettingValue(SettingsData.SETTING_USE_REQUEST_ITEM, SETTING_REQUEST_ITEM_NCIP)) {
+            if (hostLMSService.isManualCancelRequestItem()) {
+                log.debug("Sending CancelRequestItem");
+                Map cancelRequestItemResult = hostLMSService.cancelRequestItem(request, request.hrid);
+                log.debug("Result of CancelRequestItem is ${cancelRequestItemResult}");
+            }
+        }
 
         // Now set the  audit message
         actionResultDetails.auditMessage = 'Request manually flagged unable to supply';
