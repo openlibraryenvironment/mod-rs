3a1e8f811a94d95f82e93d5f91b9bcac964c3076
Kurt Nordstrom <kurt.e.nordstrom@gmail.com>
2024-04-26 14:18:32 -0400

    Add missing imports/declarations

--- service/grails-app/services/org/olf/rs/statemodel/actions/ActionResponderSupplierCannotSupplyService.groovy
+++ service/grails-app/services/org/olf/rs/statemodel/actions/ActionResponderSupplierCannotSupplyService.groovy
@@ -1,6 +1,8 @@
-package org.olf.rs.statemodel.actions;
+package org.olf.rs.statemodel.actions
 
+import org.olf.rs.HostLMSService;
 import org.olf.rs.PatronRequest
+import org.olf.rs.SettingsService
 import org.olf.rs.referenceData.SettingsData;
 import org.olf.rs.statemodel.AbstractAction;
 import org.olf.rs.statemodel.ActionResultDetails;
@@ -12,6 +14,11 @@ import org.olf.rs.statemodel.Actions;
  *
  */
 public class ActionResponderSupplierCannotSupplyService extends AbstractAction {
+
+    private static final String SETTING_REQUEST_ITEM_NCIP = "ncip";
+
+    SettingsService settingsService;
+    HostLMSService hostLMSService;
 
     @Override
     String name() {
