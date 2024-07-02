6a791aee519b715d2ebd298bafca270c5c21ac4b
Kurt Nordstrom <kurt.e.nordstrom@gmail.com>
2024-07-02 17:07:49 -0400

    Null out blank firstName and surname results in lookupPatron data (#733)

--- service/grails-app/services/org/olf/rs/hostlms/BaseHostLMSService.groovy
+++ service/grails-app/services/org/olf/rs/hostlms/BaseHostLMSService.groovy
@@ -507,6 +507,13 @@ public abstract class BaseHostLMSService implements HostLMSActions {
         result.userid = response.opt('userId') ?: response.opt('userid')
         result.givenName = response.opt('firstName')
         result.surname = response.opt('lastName')
+        //Check for blank (but not null) values
+        ["surname", "givenName"].each( {
+            if (result[it]?.isEmpty()) {
+                result[it] = null;
+            }
+        });
+
         if (response.has('electronicAddresses')) {
           JSONArray ea = response.getJSONArray('electronicAddresses')
           // We've had emails come from a key "emailAddress" AND "mailTo" in the past, check in emailAddress first and then mailTo as backup
