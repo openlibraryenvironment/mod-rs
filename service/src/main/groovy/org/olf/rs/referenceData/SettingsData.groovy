11d64cafde3be45211b7238a25b64ef630ac63a4
EddiTim <150277295+EddiTim@users.noreply.github.com>
2024-10-09 18:06:52 +0300

    Remove duplicating Automatic fee setting. (#842)
    * Remove duplicating Automatic fee setting.
    
    * Remove obsolete settings key and change ConnectionDetailsNCIP logic for checking if automatic fee is needed.

--- service/src/main/groovy/org/olf/rs/referenceData/SettingsData.groovy
+++ service/src/main/groovy/org/olf/rs/referenceData/SettingsData.groovy
@@ -181,7 +181,6 @@ public class SettingsData {
 
     // Automatic fees settings
     public static final String SETTING_REQUEST_SERVICE_TYPE  = 'request_service_type';
-    public static final String SETTING_AUTOMATIC_FEES        = 'automatic_fees';
 
     public static final String SETTING_FILE_STORAGE_ENGINE           = 'storageEngine';
     public static final String SETTING_FILE_STORAGE_S3_ENDPOINT      = 'S3Endpoint';
@@ -261,6 +260,7 @@ public class SettingsData {
             deleteByKey('shelvingLocations.feature_flag')
             deleteByKey('ncip_use_default_patron_fee')
             deleteByKey('automaticFees.feature_flag')
+            deleteByKey('automatic_fees')
 
             ensureAppSetting(SETTING_Z3950_SERVER_ADDRESS, SECTION_Z3950, SETTING_TYPE_STRING);
             ensureAppSetting(SETTING_Z3950_PROXY_ADDRESS, SECTION_Z3950, SETTING_TYPE_STRING, null, 'http://reshare-mp.folio-dev.indexdata.com:9000');
@@ -409,10 +409,6 @@ public class SettingsData {
 
             // Feature flag values for Settings sections keys
             ensureAppSetting(SETTING_FEATURE_FLAG_STATE_ACTION_CONFIGURATION_COMBINE_FILL_AND_SHIP, SECTION_FEATURE_FLAGS, SETTING_TYPE_STRING,  RefdataValueData.VOCABULARY_FEATURE_FLAG, null, null, true);
-
-            ensureAppSetting(SETTING_AUTOMATIC_FEES, SECTION_AUTOMATIC_FEES, SETTING_TYPE_REF_DATA,  ProtocolReferenceDataValue.CATEGORY_SERVICE_TYPE, null, ProtocolReferenceDataValue.lookup(ProtocolReferenceDataValue.CATEGORY_SERVICE_TYPE, ProtocolReferenceDataValue.SERVICE_TYPE_COPY).value)
-            ensureAppSetting(SETTING_AUTOMATIC_FEES, SECTION_AUTOMATIC_FEES, SETTING_TYPE_REF_DATA,  ProtocolReferenceDataValue.CATEGORY_SERVICE_TYPE, null, ProtocolReferenceDataValue.lookup(ProtocolReferenceDataValue.CATEGORY_SERVICE_TYPE, ProtocolReferenceDataValue.SERVICE_TYPE_LOAN).value)
-            ensureAppSetting(SETTING_AUTOMATIC_FEES, SECTION_AUTOMATIC_FEES, SETTING_TYPE_REF_DATA,  ProtocolReferenceDataValue.CATEGORY_SERVICE_TYPE, null, ProtocolReferenceDataValue.lookup(ProtocolReferenceDataValue.CATEGORY_SERVICE_TYPE, ProtocolReferenceDataValue.SERVICE_TYPE_COPY_OR_LOAN).value)
 
         } catch (Exception e) {
             log.error('Exception thrown while loading settings', e);
