876e45524e455208c363abe47421e43068b726b9
Kurt Nordstrom <kurt.e.nordstrom@gmail.com>
2025-01-29 10:42:38 -0500

    Modify Horizon Host LMS Item Location logic (#744) (#904)

--- service/src/test/groovy/org/olf/rs/hostlms/HorizonHostLMSServiceSpec.groovy
+++ service/src/test/groovy/org/olf/rs/hostlms/HorizonHostLMSServiceSpec.groovy
@@ -9,15 +9,19 @@ import spock.lang.Specification;
 class HorizonHostLMSServiceSpec extends Specification implements ServiceUnitTest<HorizonHostLMSService> {
     def 'extractAvailableItemsFrom'() {
         setup:
-        def parsedSample = new XmlSlurper().parseText(new File('src/test/resources/zresponsexml/horizon-jhu.xml').text);
+        def parsedSample = new XmlSlurper().parseText(new File("src/test/resources/zresponsexml/${zResponseFile}").text);
 
         when: 'We extract holdings'
         def result = service.extractAvailableItemsFrom(parsedSample, null, new DoNothingHoldingLogDetails());
 
         then:
-        def resultJson = JsonOutput.toJson(result.first());
-        result.size() == 1;
-        resultJson == '{"temporaryShelvingLocation":null,"itemId":null,"temporaryLocation":null,"shelvingLocation":null,"callNumber":"SH 349 .M34 1992","reason":null,"shelvingPreference":null,"preference":null,"location":"Eisenhower","itemLoanPolicy":"Available"}';
+        def resultJson = JsonOutput.toJson(result);
+        resultJson == validResultJson;
+
+        where:
+        zResponseFile              | validResultJson
+        'horizon-jhu.xml'          | '[{"temporaryShelvingLocation":null,"itemId":null,"temporaryLocation":null,"shelvingLocation":null,"callNumber":"SH 349 .M34 1992","reason":null,"shelvingPreference":null,"preference":null,"location":"Eisenhower","itemLoanPolicy":"MSEL Books"}]'
+        'horizon-hennepin.xml'     | '[{"temporaryShelvingLocation":null,"itemId":null,"temporaryLocation":null,"shelvingLocation":"Adult Fiction Book (Stacks)","callNumber":"YEVTUSH","reason":null,"shelvingPreference":null,"preference":null,"location":"Minneapolis Central (1st floor General)","itemLoanPolicy":"Checked In"}]'
     }
 }
 
