4431df2c2d02a6227aeca1d235504e48de29bbd3
Jason Skomorowski <jason@indexdata.com>
2025-08-08 17:02:27 -0400

    Improve test, fix the delimited string I missed

--- service/grails-app/services/org/olf/rs/statemodel/actions/iso18626/ActionISO18626RequesterService.groovy
+++ service/grails-app/services/org/olf/rs/statemodel/actions/iso18626/ActionISO18626RequesterService.groovy
@@ -113,7 +113,7 @@ public abstract class ActionISO18626RequesterService extends ActionISO18626Servi
                     // We have a single string, this is the usual standard case and should be handled as a single request volume
                     // Check if a RequestVolume exists for this itemId, and if not, create one
                     // At this point we have an itemId of the form "<name>,<id>,<callNumber>"
-                    Matcher matcher = itemId =~ /(.*),(.*),(.*)/
+                    Matcher matcher = itemId =~ /(.*)##(.*)##(.*)/
                     String iidId = itemId
                     String iidName = itemId
                     String iidCallNumber = null
