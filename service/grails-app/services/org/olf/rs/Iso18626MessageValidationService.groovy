ece89ea56643d1b606f2eb64c722af3ef18af64f
Kurt Nordstrom <kurt.e.nordstrom@gmail.com>
2025-09-05 16:23:49 -0400

    Use a new validator object whenever we check a schema (#1023)
    * Use a new validator object whenever we check a schema
    
    * Re-use Schema object in validator service

--- service/grails-app/services/org/olf/rs/Iso18626MessageValidationService.groovy
+++ service/grails-app/services/org/olf/rs/Iso18626MessageValidationService.groovy
@@ -11,24 +11,23 @@ import javax.xml.validation.Validator
 
 @Slf4j
 class Iso18626MessageValidationService {
-
-    Validator validator = null
+    Schema schema = null; //Schema should be thread-safe, according to java docs
 
     void validateAgainstXSD(String xml) {
         try {
-            if (validator == null) {
+            if (schema == null) {
                 SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI)
-                Schema schema = schemaFactory.newSchema(this.class.classLoader.getResource('xsd/ISO-18626-v1_2.xsd'))
-                validator = schema.newValidator()
+                schema = schemaFactory.newSchema(this.class.classLoader.getResource('xsd/ISO-18626-v1_2.xsd'))
             }
+            Validator validator = schema.newValidator() //Validator is NOT thread-safe, cannot be shared
             validator.validate(new StreamSource(new StringReader(xml)))
             log.debug("XSD validation successful")
         } catch (SAXException e) {
             log.error("XSD schema validation failed", e)
             throw e
         } catch (Exception e) {
-            log.error("Error attempting to validate iso18626 message against schema  ${e.getLocalizedMessage()}");
-            throw e;
+            log.error("Error attempting to validate iso18626 message against schema  ${e.getLocalizedMessage()}")
+            throw e
         }
     }
 }
