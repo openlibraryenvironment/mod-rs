f454c328fc1b4c39b419a854391fc1aba02505d8
skomorokh <skomorokh@users.noreply.github.com>
2025-09-12 09:44:36 -0400

    Handle Unfilled messages when conditions pending ILLDEV-215 (#1029)

--- service/src/main/groovy/org/olf/rs/referenceData/ActionEventResultData.groovy
+++ service/src/main/groovy/org/olf/rs/referenceData/ActionEventResultData.groovy
@@ -158,6 +158,16 @@ public class ActionEventResultData {
         nextActionEvent: null
     ];
 
+    private static Map requesterISO18626UnfilledContinue = [
+        code: 'requesterISO18626UnfilledContinue',
+        description: 'An incoming ISO-18626 message for the requester has said that the status is Unfilled but the broker expects to continue to next supplier',
+        result: true,
+        status: Status.PATRON_REQUEST_REQUEST_SENT_TO_SUPPLIER,
+        qualifier: 'UnfilledContinue',
+        saveRestoreState: null,
+        nextActionEvent: null
+    ];
+
     private static Map responderISO18626AgreeConditions = [
         code: 'responderISO18626AgreeConditions',
         description: 'Requester has said they want to agree to the conditions',
@@ -1048,6 +1058,8 @@ public class ActionEventResultData {
         model: StateModel.MODEL_REQUESTER,
         results: [
             requesterISO18626Loaned,
+            requesterISO18626Unfilled,
+            requesterISO18626UnfilledContinue,
             defaultNoStatusChangeOK
         ]
     ];
@@ -1061,6 +1073,7 @@ public class ActionEventResultData {
             requesterISO18626Loaned,
             requesterISO18626Unfilled,
             requesterISO18626UnfilledTransfer,
+            requesterISO18626UnfilledContinue,
             defaultNoStatusChangeOK
         ]
     ];
@@ -1627,6 +1640,7 @@ public class ActionEventResultData {
         requesterISO18626Conditional,
         digitalReturnableRequesterISO18626Loaned,
         requesterISO18626Unfilled,
+        requesterISO18626UnfilledContinue,
         defaultNoStatusChangeOK
       ]
     ];
