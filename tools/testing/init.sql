872d582240f5ffda58343336234fd59ad72d5b87
ihardy <ian@indexdata.com>
2026-02-24 14:22:27 -0500

    remove conditions for role creation

--- tools/testing/init.sql
+++ tools/testing/init.sql
@@ -1,23 +1,14 @@
 select 1;
 
-DO $$
-BEGIN
-  IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname='folio_admin') THEN
-    CREATE ROLE folio_admin LOGIN PASSWORD 'folio_admin' SUPERUSER CREATEDB CREATEROLE;
-  END IF;
-  IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname='crosslink') THEN
-    CREATE ROLE crosslink LOGIN PASSWORD 'crosslink';
-  END IF;
-  IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname='crosslink_broker') THEN
-    CREATE ROLE crosslink_broker NOLOGIN;
-  END IF;
-END $$;
+CREATE ROLE folio_admin LOGIN PASSWORD 'folio_admin' SUPERUSER CREATEDB CREATEROLE;
+CREATE ROLE crosslink LOGIN PASSWORD 'crosslink';
+CREATE ROLE crosslink_broker NOLOGIN;
 
 DROP DATABASE IF EXISTS okapi_modules;
 CREATE DATABASE okapi_modules OWNER folio_admin;
 GRANT ALL PRIVILEGES ON DATABASE okapi_modules TO folio_admin;
 
--- BROKER setup
+-- BROKER SETUP
 \connect okapi_modules
 GRANT crosslink_broker TO crosslink;
 GRANT CONNECT ON DATABASE okapi_modules TO crosslink;
@@ -29,5 +20,6 @@ GRANT USAGE, CREATE ON SCHEMA crosslink_broker TO crosslink;
 
 ALTER ROLE crosslink IN DATABASE okapi_modules
 SET search_path = crosslink_broker, pg_temp;
+-- END BROKER SETUP
 
 CREATE EXTENSION IF NOT EXISTS pg_trgm WITH SCHEMA public;
