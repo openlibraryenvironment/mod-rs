92db150a075aca5765f872b019c618f3aba568c4
Ian Hardy <ian.c.hardy@gmail.com>
2026-02-24 14:44:28 -0500

    adjust broker test database (#1055)
    * capture broker container log
    
    * change db for broker
    
    * update mod-rs for new crosslink schema setup
    
    * more changes to init.sql
    
    * remove conditions for role creation
    
    * update apple docker-compose

--- tools/testing/init.sql
+++ tools/testing/init.sql
@@ -1,11 +1,25 @@
 select 1;
 
-CREATE USER folio_admin WITH PASSWORD 'folio_admin';
-DROP DATABASE if exists okapi_modules;
-CREATE DATABASE okapi_modules;
-GRANT ALL PRIVILEGES ON DATABASE okapi_modules to folio_admin;
+CREATE ROLE folio_admin LOGIN PASSWORD 'folio_admin' SUPERUSER CREATEDB CREATEROLE;
+CREATE ROLE crosslink LOGIN PASSWORD 'crosslink';
+CREATE ROLE crosslink_broker NOLOGIN;
 
-ALTER USER folio_admin CREATEDB;
-ALTER USER folio_admin CREATEROLE;
-ALTER USER folio_admin WITH SUPERUSER;
-CREATE EXTENSION IF NOT EXISTS pg_trgm;
+DROP DATABASE IF EXISTS okapi_modules;
+CREATE DATABASE okapi_modules OWNER folio_admin;
+GRANT ALL PRIVILEGES ON DATABASE okapi_modules TO folio_admin;
+
+-- BROKER SETUP
+\connect okapi_modules
+GRANT crosslink_broker TO crosslink;
+GRANT CONNECT ON DATABASE okapi_modules TO crosslink;
+
+CREATE SCHEMA IF NOT EXISTS crosslink_broker AUTHORIZATION crosslink_broker;
+
+REVOKE ALL ON SCHEMA public FROM crosslink;
+GRANT USAGE, CREATE ON SCHEMA crosslink_broker TO crosslink;
+
+ALTER ROLE crosslink IN DATABASE okapi_modules
+SET search_path = crosslink_broker, pg_temp;
+-- END BROKER SETUP
+
+CREATE EXTENSION IF NOT EXISTS pg_trgm WITH SCHEMA public;
